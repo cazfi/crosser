c7562a77bfd07f5a5afb7ded974bb64b0f5dede1
Marko Lindqvist <cazfi74@gmail.com>
2008-05-15 22:56:53 +0000

    Added stepset.sh script fragment to set STEP -variables.

---
+++ steps/stepset.sh
@@ -0,0 +1,21 @@
+# No shebang. This file must be sourced to actual script
+
+# stepfuncs.sh: Functions handling Crosser steps.
+#
+# (c) 2008 Marko Lindqvist
+#
+# This program is licensed under Gnu General Public License version 2.
+
+for _STEP in $STEPLIST
+do
+  case $_STEP in
+    native)
+      STEP_NATIVE=yes ;;
+    chain)
+      STEP_CHAIN=yes ;;
+    win)
+      STEP_WIN=yes ;;
+    *)
+      log_error "Unknown step $_STEP in stepset.sh" ;;
+  esac
+done
