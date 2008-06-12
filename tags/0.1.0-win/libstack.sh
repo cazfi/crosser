71a28bc8defd18deae4c2efd7555e2f111641565
cazfi <cazfi@ed8cdf8a-1376-463e-89c1-a3058e460e0f>
2008-06-12 17:21:48 +0000

    Release 0.1.0-win

---
+++ tags/0.1.0-win/libstack.sh
@@ -0,0 +1,494 @@
+#!/bin/bash
+
+# libstack.sh: Cross-compile set of libraries for Windows target.
+#
+# (c) 2008 Marko Lindqvist
+#
+# This program is licensed under Gnu General Public License version 2.
+#
+
+MAINDIR=$(cd $(dirname $0) ; pwd)
+
+# These have hardcoded value 'off' for a reason - enabling them will not work
+export CROSSER_OPTION_JPEG=off
+export CROSSER_OPTION_CUPS=off
+
+if test "x$1" = "x-h" || test "x$1" = "x--help"
+then
+  echo "Usage: $(basename $0) [[-h|--help]|[-v|--version]|[setup]] [install prefix] [versionset]"
+  exit 0
+fi
+
+if test "x$1" = "x-v" || test "x$1" = "x--version"
+then
+  echo "Windows library stack builder for Crosser $CROSSER_VERSION"
+  exit 0
+fi
+
+# In order to give local setup opportunity to override versions,
+# we have to load versionset before build_setup.conf
+# helpers.sh requires environment to be set up by build_setup.conf.
+if test "x$3" != "x" ; then
+  VERSIONSET="$3"
+else
+  VERSIONSET="current"
+fi
+if test -e $MAINDIR/setups/$VERSIONSET.versions
+then
+  . $MAINDIR/setups/$VERSIONSET.versions
+else
+  # Versions being unset do not prevent loading of build_setup.conf and helper.sh,
+  # resulting environment would just be unusable for building.
+  # We are not going to build anything, but just issuing error message - and for
+  # that we read log_error from helpers.sh
+  ERR_MSG="Cannot find versionset \"$VERSIONSET.versions\""
+fi
+
+. $MAINDIR/build_setup.conf
+. $MAINDIR/scripts/helpers.sh
+
+if ! log_init
+then
+  echo "Cannot setup logging!" >&2
+  exit 1
+fi
+
+if test "x$ERR_MSG" != "x"
+then
+  log_error "$ERR_MSG"
+  exit 1
+fi
+
+if test "x$2" != "x"
+then
+  PREFIX="$2"
+fi
+
+# $1 - Component
+# $2 - Version
+# $3 - Extra configure options
+build_component_srcdir()
+{
+  build_component_full "$1" "$2" "$3" "src"
+}
+
+# $1 - Component
+# $2 - Version
+# $3 - Extra configure options
+build_component()
+{
+  build_component_full "$1" "$2" "$3"
+}
+
+# $1 - Component
+# $2 - Version
+# $3 - Extra configure options
+# $4 - Build in src dir?
+# $5 - Overwrite libtool
+build_component_full()
+{
+  log_packet "$1"
+
+  SUBDIR="$(src_subdir $1 $2)"
+
+  if test "x$SUBDIR" = "x"
+  then
+    log_error "Cannot find srcdir for $1 version $2"
+    return 1
+  fi
+
+  if test "x$4" != "xsrc" ; then
+    BUILDDIR="$MAINBUILDDIR/$1"
+    if ! mkdir -p "$BUILDDIR"
+    then
+      log_error "Failed to create directory $BUILDDIR"
+      return 1
+    fi
+    cd $BUILDDIR
+    SRCDIR="$MAINSRCDIR/$SUBDIR"
+  else
+    if ! cd $MAINSRCDIR/$SUBDIR
+    then
+      log_error "Cannot change to directory $MAINSRCDIR/$SUBDIR"
+      return 1
+    fi
+    SRCDIR="."
+  fi
+
+  CONFOPTIONS="--prefix=$PREFIX --build=$BUILD --host=$TARGET --target=$TARGET $3"
+
+  export CPPFLAGS="-isystem $PREFIX/include -isystem $TGT_HEADERS $TGT_MARCH $USER_CPPFLAGS"
+  export LDFLAGS="-L$PREFIX/lib $USER_LDFLAGS"
+
+  log_write 1 "Configuring: $1"
+  log_write 3 "  Options: \"$CONFOPTIONS\""
+  log_flags
+
+  if ! $SRCDIR/configure $CONFOPTIONS >>$MAINLOGDIR/stdout.log 2>>$MAINLOGDIR/stderr.log
+  then
+    log_error "Configure for $1 failed"
+    return 1
+  fi
+
+  if test "x$5" = "xoverwrite" ; then
+    log_write 2 "Copying working libtool to $1"
+    if ! cp $MAINBUILDDIR/libtool/libtool .
+    then
+      log_error "Failed to copy libtool"
+      return 1
+    fi
+  elif test "x$5" != "x" ; then
+    log_error "Illegal libtool overwrite parameter $5"
+    return 1
+  fi
+
+  log_write 1 "Building $1"
+  log_write 3 "  Make targets: [default] install"
+
+  if ! make  >>$MAINLOGDIR/stdout.log 2>>$MAINLOGDIR/stderr.log
+  then
+    log_error "Make for $1 failed"
+    return 1
+  fi
+
+  if ! make install >>$MAINLOGDIR/stdout.log 2>>$MAINLOGDIR/stderr.log
+  then
+    log_error "Install for $1 failed"
+    return 1
+  fi
+}
+
+build_zlib()
+{
+  log_packet "$1"
+
+  SUBDIR="$(src_subdir $1 $2)"
+
+  if test "x$SUBDIR" = "x"
+  then
+    log_error "Cannot find srcdir for $1 version $2"
+    return 1
+  fi
+
+  export CC=$TARGET-gcc
+  export RANLIB=$TARGET-ranlib
+  export AR=$TARGET-ar
+
+  if ! cd $MAINSRCDIR/$SUBDIR
+  then
+    log_error "Cannot change to directory $MAINSRCDIR/$SUBDIR"
+    return 1
+  fi
+
+  export CPPFLAGS="-isystem $PREFIX/include -isystem $TGT_HEADERS $USER_CPPFLAGS"
+  export LDFLAGS="-L$PREFIX/lib $USER_LDFLAGS"
+
+  CONFOPTIONS="--prefix=$PREFIX --shared $3"
+
+  # TODO: zlib build doesn't like this variable, check why.
+  unset TARGET_ARCH
+
+  log_write 1 "Configuring: $1"
+  log_write 3 "  Options: \"$CONFOPTIONS\""
+  log_flags
+
+  if ! ./configure $CONFOPTIONS >>$MAINLOGDIR/stdout.log 2>>$MAINLOGDIR/stderr.log
+  then
+    log_error "Configure for $1 failed"
+    return 1
+  fi
+
+  log_write 1 "Building $1"
+  log_write 3 "  Make targets: [default] install"
+
+  if ! make >>$MAINLOGDIR/stdout.log 2>>$MAINLOGDIR/stderr.log
+  then
+    log_error "Make for $1 failed"
+    return 1
+  fi
+
+  if ! make install  >>$MAINLOGDIR/stdout.log 2>>$MAINLOGDIR/stderr.log
+  then
+    log_error "Install for $1 failed"
+    return 1
+  fi
+
+  if ! cp $PREFIX/lib/libz.dll* $PREFIX/bin/ ||
+     ! mv $PREFIX/lib/libz.a    $PREFIX/bin/
+  then
+    log_error "Failed to move libz dll:s to correct directory"
+    return 1
+  fi
+}
+
+update_aux_file()
+{
+  # Update only those files that already exist in target directory
+  if test -e $MAINSRCDIR/$1/$2 ; then
+    log_write 2 "Updating $2"
+    if ! cp $MAINDIR/scripts/aux/$2 $MAINSRCDIR/$1/
+    then
+      return 1
+    fi
+  fi
+}
+
+update_aux_files() {
+
+  log_write 1 "Updating auxiliary files for $1"
+
+  SUBDIR="$(src_subdir $1 $2)"
+
+  if "x$SUBDIR" = "x"
+  then
+    log_error "Cannot find srcdir for $1 version $2"
+    return 1
+  fi
+
+  if ! update_aux_file $SUBDIR config.guess ||
+     ! update_aux_file $SUBDIR config.sub   ||
+     ! update_aux_file $SUBDIR install-sh   ||
+     ! update_aux_file $SUBDIR ltmain.sh
+  then
+    log_error "Failed to update auxiliary files in directory $SUBDIR"
+    return 1
+  fi
+}
+
+cd $(dirname $0)
+
+if ! source $MAINSETUPDIR/native.sh ; then
+  log_error "Failed to read $MAINSETUPDIR/native.sh"
+  exit 1
+fi
+NATIVE_ARCH="$TMP_ARCH"
+NATIVE_OS="$TMP_OS"
+BUILD="$NATIVE_ARCH-$NATIVE_OS"
+
+if test "x$1" != "x" ; then
+  SETUP="$1"
+else
+  SETUP="win32"
+  log_write 1 "No setup selected, defaulting to \"$SETUP\""
+fi
+
+if ! test -e "$MAINSETUPDIR/$SETUP.conf" ; then
+  log_error "Can't find setup \"$SETUP.conf\""
+  exit 1
+fi
+source "$MAINSETUPDIR/$SETUP.conf"
+
+TARGET="$TARGET_ARCH-$TARGET_OS"
+
+if test -d /usr/$TARGET/include
+then
+  export TGT_HEADERS=/usr/$TARGET/include
+fi
+
+TGT_MARCH="-march=$TARGET_ARCH"
+
+export LIBC_MODE="none"
+
+export PREFIX=$(setup_prefix_default "$(pwd)/install-<TARGET>-<DATE>" "$PREFIX")
+
+export USER_CPPFLGS="$CPPFLAGS"
+export USER_LDFLAGS="$LDFLAGS"
+export USER_CFLAGS="$CFLAGS"
+export USER_CXXFLAGS="$CXXFLAGS"
+
+log_write 2 "Install:    \"$PREFIX\""
+log_write 2 "Src:        \"$MAINSRCDIR\""
+log_write 2 "Log:        \"$MAINLOGDIR\""
+log_write 2 "Build:      \"$MAINBUILDDIR\""
+log_write 2 "Versionset: \"$VERSIONSET\""
+
+if ! remove_dir "$MAINSRCDIR"   ||
+   ! remove_dir "$MAINBUILDDIR" ||
+   ! remove_dir "$PREFIX"
+then
+  log_error "Failed to remove old directories"
+  exit 1
+fi
+
+if ! mkdir -p $MAINSRCDIR
+then
+  log_error "Cannot create directory $MAINSRCDIR"
+  exit 1
+fi
+
+if ! mkdir -p $MAINBUILDDIR
+then
+  log_error "Cannot create directory $MAINBUILDDIR"
+  exit 1
+fi
+
+if ! mkdir -p $PREFIX/man/man1
+then
+  log_error "Cannot create target directory hierarchy under $PREFIX"
+  exit 1
+fi
+
+export CROSSER_DOWNLOAD=demand
+
+if ! unpack_component libtool    $VERSION_LIBTOOL      ||
+   ! unpack_component libiconv   $VERSION_ICONV        ||
+   ! unpack_component zlib       $VERSION_ZLIB         ||
+   ! unpack_component libpng     $VERSION_PNG          ||
+   ! unpack_component gettext    $VERSION_GETTEXT      ||
+   ! unpack_component glib       $VERSION_GLIB         ||
+   ! unpack_component tiff       $VERSION_TIFF         ||
+   ! unpack_component expat      $VERSION_EXPAT        ||
+   ! unpack_component freetype   $VERSION_FREETYPE     ||
+   ! unpack_component fontconfig $VERSION_FONTCONFIG   ||
+   ! unpack_component cairo      $VERSION_CAIRO        ||
+   ! unpack_component pango      $VERSION_PANGO        ||
+   ! unpack_component atk        $VERSION_ATK          ||
+   ! unpack_component gtk+       $VERSION_GTK          ||
+   ! unpack_component SDL        $VERSION_SDL          ||
+   ! unpack_component SDL_image  $VERSION_SDL_IMAGE
+then
+  log_error "Unpacking failed"
+  exit 1
+fi
+
+if test "x$CROSSER_OPTION_JPEG" = "xon" &&
+   ! unpack_component libjpeg6b  $VERSION_JPEG
+then
+  log_error "Unpacking failed"
+  exit 1
+fi
+
+if test "x$CROSSER_OPTION_CUPS" = "xon" &&
+   ! unpack_component cups       $VERSION_CUPS "" cups-$VERSION_CUPS-source
+then
+  log_error "Unpacking failed"
+  exit 1
+fi      
+
+if ! patch_src zlib                           zlib_seeko                ||
+   ! patch_src zlib                           zlib_fpiccheck            ||
+   ! patch_src zlib                           zlib_nolibc               ||
+   ! patch_src zlib                           zlib_dllext               ||
+   ! patch_src glib-$VERSION_GLIB             glib_acsizeof             ||
+   ! patch_src glib-$VERSION_GLIB             glib_stackgrow            ||
+   ! patch_src tiff-$VERSION_TIFF             tiff_config_headers       ||
+   ! patch_src freetype-$VERSION_FREETYPE     freetype_dll              ||
+   ! patch_src fontconfig-$VERSION_FONTCONFIG fontconfig_buildsys_flags ||
+   ! patch_src gtk+-$VERSION_GTK              gtk_check_cxx
+then
+  log_error "Patching failed"
+  exit 1
+fi
+
+if ! ( ! cmp_versions $VERSION_TIFF 4.0.0alpha ||
+       patch_src tiff-$VERSION_TIFF tiff4alpha_largefile )
+then
+  log_error "Patching failed"
+  exit 1
+fi
+
+if is_smaller_version $VERSION_GTK 2.12.10
+then
+  if patch_src gtk+-$VERSION_GTK             gtk_blddir
+  then
+    log_error "Patching failed"
+    exit 1
+  fi
+fi
+
+if test "x$CROSSER_OPTION_JPEG" = "xon"
+then
+  if ! patch_src libjpeg6b                      jpeg_ltmain               ||
+     ! patch_src libjpeg6b                      jpeg_ltcompile            ||
+     ! patch_src libjpeg6b                      jpeg_ar                   ||
+     ! patch_src libjpeg6b                      jpeg_noundef
+  then
+    log_error "Patching failed"
+    exit 1
+  fi
+fi
+
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+
+if is_smaller_version $VERSION_TIFF 4.0.0beta1
+then
+  log_write 1 "Removing upstream libtiff config"
+  if ! rm $MAINSRCDIR/tiff-$VERSION_TIFF/libtiff/tiffconf.h
+  then
+    log_error "Failed to remove old tiffconf.h"
+    exit 1
+  fi
+fi
+
+if ! build_component   libtool    $VERSION_LIBTOOL          ||
+   ! build_component   libiconv   $VERSION_ICONV            ||
+   ! build_zlib        zlib                                 ||
+   ! build_component   libpng     $VERSION_PNG              ||
+   ! (export LIBS="-liconv" && build_component gettext  $VERSION_GETTEXT) ||
+   ! autogen_component glib       $VERSION_GLIB  "autoconf" ||
+   ! build_component   glib       $VERSION_GLIB
+then
+  log_error "Build failed"
+  exit 1
+fi
+
+if test "x$CROSSER_OPTION_JPEG" = "xon"
+then
+  if ! update_aux_files     libjpeg6b  $VERSION_JPEG          ||
+     ! build_component_full libjpeg6b  $VERSION_JPEG "--enable-shared" "" "overwrite"
+  then
+    log_error "Build failed"
+    exit 1
+  fi
+else
+  CONF_JPEG_TIFF="--disable-jpeg"
+  CONF_JPEG_GTK="--without-libjpeg"
+fi
+
+if is_minimum_version $VERSION_GTK 2.13.0
+then
+  CONF_JPEG_GTK="$CONF_JPEG_GTK --without-libjasper"
+fi
+
+if ! autogen_component tiff       $VERSION_TIFF             ||
+   ! build_component   tiff       $VERSION_TIFF "$CONF_JPEG_TIFF"             ||
+   ! build_component   expat      $VERSION_EXPAT            ||
+   ! autogen_component freetype   $VERSION_FREETYPE         ||
+   ! build_component   freetype   $VERSION_FREETYPE         ||
+   ! autogen_component fontconfig $VERSION_FONTCONFIG       ||
+   ! build_component   fontconfig $VERSION_FONTCONFIG       \
+     "--with-freetype-config=$PREFIX/bin/freetype-config --with-arch=$TARGET" ||
+   ! build_component   cairo      $VERSION_CAIRO "--disable-xlib" ||
+   ! build_component   pango      $VERSION_PANGO                  ||
+   ! build_component   atk        $VERSION_ATK                    ||
+   ! autogen_component gtk+       $VERSION_GTK                    ||
+   ! build_component   gtk+       $VERSION_GTK \
+     "--disable-cups --disable-explicit-deps $CONF_JPEG_GTK"
+then
+  log_error "gtk+ stack build failed"
+  exit 1
+fi
+
+if test "x$AUTOWINE" = "xyes" ; then
+  log_write 1 "Creating configuration files"
+  if ! mkdir -p $PREFIX/etc/pango ||
+     ! $PREFIX/bin/pango-querymodules.exe > $PREFIX/etc/pango/pango.modules ||
+     ! $PREFIX/bin/gdk-pixbuf-query-loaders.exe > $PREFIX/etc/gtk-2.0/gdk-pixbuf.loaders
+  then
+    log_error "Failed to create configuration files in wine."
+    exit 1
+  fi
+else
+  log_write 1 "IMPORTANT: Remember to create configuration files when installing to target"
+fi
+
+if ! build_component   SDL        $VERSION_SDL       ||
+   ! build_component   SDL_image  $VERSION_SDL_IMAGE
+then
+  log_error "SDL stack build failed"
+  exit 1
+fi
+
+generate_setup_scripts "$PREFIX"
+
+log_write 1 "SUCCESS"
---
+++ tags/0.1.0-win/scripts/helpers.sh
@@ -0,0 +1,472 @@
+# No shebang, this script is not executed, but sourced.
+
+# helpers.sh: Functions for Crosser
+#
+# (c) 2008 Marko Lindqvist
+#
+# This program is licensed under Gnu General Public License version 2.
+
+if test "x$MAINDIR" = "x"
+then
+  echo "helpers.sh: Mandatory environments variables missing! Have you sourced proper settings already?" >&2
+  exit 1
+fi
+
+CROSSER_VERSION=$(tail -n 1 $MAINDIR/Version)
+BUILD_DATE=$(date +"%d%m%y")
+
+if test "x$LOGLEVEL_STDOUT" = "x" ; then
+  LOGLEVEL_STDOUT=2
+fi
+if test "x$LOGLEVEL_FILE" = "x" ; then
+  LOGLEVEL_FILE=4
+fi
+
+log_init() {
+  if ! mkdir -p $MAINLOGDIR
+  then
+    echo "Failed to create logdir \"$MAINLOGDIR\"" >&2
+    return 1
+  fi
+
+  log_write 0 "== Build starts =="
+  rm -f $MAINLOGDIR/stderr.log
+  rm -f $MAINLOGDIR/stdout.log
+}
+
+# Call this when starting build of new packet
+# $1 - Packet name
+log_packet() {
+  # Create lines separating output from one packet build from the other
+  echo "**** $1 ****" >> $MAINLOGDIR/stdout.log
+  echo "**** $1 ****" >> $MAINLOGDIR/stderr.log
+}
+
+log_write() {
+  DSTAMP=$(date +"%d.%m %H:%M")
+
+  if test "x$1" = "x0" ; then
+    echo >> $MAINLOGDIR/main.log
+    log_write 1 "======================================"
+  fi
+
+  if test $1 -le $LOGLEVEL_FILE ; then
+    echo -e "$DSTAMP $STEP:$STEPADD $2" >> $MAINLOGDIR/main.log
+  fi
+
+  if test $1 -le $LOGLEVEL_STDOUT ; then
+    echo -e "$DSTAMP $STEP:$STEPADD $2"
+  fi
+}
+
+log_error() {
+  log_write 1 "$1"
+  echo "$1" >&2
+}
+
+log_flags() {
+  log_write 4 "  CPPFLAGS: \"$CPPFLAGS\""
+  log_write 4 "  CFLAGS:   \"$CFLAGS\""
+  log_write 4 "  CXXFLAGS: \"$CXXFLAGS\""
+  log_write 4 "  LDFLAGS:  \"$LDFLAGS\""
+  log_write 4 "  PATH:     \"$PATH\""
+}
+
+# Generate scripts which can be used to launch this compiling environment
+#
+# $1 - Target path
+generate_setup_scripts() {
+  log_write 1 "Writing setup scripts"
+
+  if ! (
+    echo "#"\!"/bin/sh"
+    echo "export CROSSER=$TARGET"
+    echo "export PATH=\"$PATH\""
+    echo "export PS1=\"Crosser:> \""
+    echo "hash -r"
+    echo "/bin/bash --norc"
+  ) > $1/setup.sh
+  then
+    log_error "Failed to create $1/setup.sh"
+    return 1
+  fi
+
+  chmod a+x $1/setup.sh
+}
+
+# Apply patch to component
+#
+# $1 - Subdir in source hierarchy to patch
+# $2 - Patch name
+patch_src() {
+  log_write 2 "Patching $1: $2.diff"
+
+  if ! patch -u -p1 -d $MAINSRCDIR/$1 < $MAINDIR/patch/$2.diff \
+       >> $MAINLOGDIR/stdout.log 2>> $MAINLOGDIR/stderr.log
+  then
+    log_error "Patching $1 with $2.diff failed"
+    return 1
+  fi
+}
+
+# Unpack component package to source directory
+#
+# $1   - Package name
+# $2   - Package version
+# [$3] - Subdir in source hierarchy
+# [$4] - Package file name base in case it's not 'name-version'
+unpack_component() {
+  if test "x$CROSSER_DOWNLOAD" = "xdemand" ; then
+    log_write 1 "Downloading $1 version $2"
+    if ! $MAINPACKETDIR/download_packets.sh --packet "$1" "$2" \
+         >>$MAINLOGDIR/stdout.log 2>>$MAINLOGDIR/stderr.log
+    then
+      log_error "Failed to download $1 version $2"
+      return 1
+    fi
+  fi
+
+  log_write 1 "Unpacking $1 version $2"
+
+  if test "x$4" != "x"
+  then
+    # Custom file name format
+    NAME_BASE="$4"
+  else
+    NAME_BASE="$1-$2"
+  fi
+
+  if test -e $MAINPACKETDIR/$NAME_BASE.tar.bz2 ; then
+    if ! tar xjf $MAINPACKETDIR/$NAME_BASE.tar.bz2 -C $MAINSRCDIR/$3
+    then
+      log_error "Unpacking $NAME_BASE.tar.bz2 failed"
+      return 1
+    fi
+  elif test -e $MAINPACKETDIR/$NAME_BASE.tar.gz ; then
+    if ! tar xzf $MAINPACKETDIR/$NAME_BASE.tar.gz -C $MAINSRCDIR/$3
+    then
+      log_error "Unpacking $NAME_BASE.tar.gz failed"
+      return 1
+    fi
+  elif test -e $MAINPACKETDIR/${1}_${2}.dsc ; then
+    if ! which dpkg-source >/dev/null ; then
+      log_error "No way to unpack debian source packages"
+      return 1
+    fi
+    if test "x$3" = "x" ; then
+      SRCDIR="$1"
+    else
+      SRCDIR="$3"
+    fi
+    if ! dpkg-source -x $MAINPACKETDIR/${1}_${2}.dsc $MAINSRCDIR/$SRCDIR
+    then
+      log_error "Unpacking $1_$2.dsc failed"
+      return 1
+    fi
+  else
+    log_error "Can't find $1 version $2 package to unpack."
+    return 1
+  fi
+}
+
+# Output subdir under source hierarchy where component lives
+#
+# $1 -   Package
+# $2 -   Version
+src_subdir() {
+  if test -d "$MAINSRCDIR/$1-$2"
+  then
+    echo "$1-$2"
+  elif test -d "$MAINSRCDIR/$1"
+  then
+    echo "$1"
+  else
+    return 1
+  fi
+}
+
+# Run set of autotools for component
+#
+# $1   - Package
+# $2   - Version
+# [$3] - List of tools to execute, "all" to force use of all tools
+autogen_component()
+{
+  log_packet "autogen $1"
+
+  SUBDIR="$(src_subdir $1 $2)"
+
+  if test "x$SUBDIR" = "x"
+  then
+    log_error "Cannot find srcdir for $1 version $2"
+    return 1
+  fi
+
+  cd $MAINSRCDIR/$SUBDIR
+
+  if test "x$3" = "x" && test -f autogen.sh ; then
+    if ! test -x autogen.sh ; then
+      log_write 1 "Making $1 autogen.sh executable"
+      chmod u+x autogen.sh
+    fi
+    if ! ./autogen.sh ; then
+      log_error "Autogen failed for $1"
+      return 1
+    fi
+  else
+    if test "x$3" = "x" || test "x$3" = "xall" ; then
+      TOOLS="aclocal automake autoconf"
+    else
+      TOOLS="$3"
+    fi
+    for TOOL in $TOOLS
+    do
+      if ! $TOOL >>$MAINLOGDIR/stdout.log 2>>$MAINLOGDIR/stderr.log
+      then
+        log_error "Autotool $TOOL failed for $1 version $2"
+        return 1
+      fi
+    done
+  fi
+}
+
+# Outputs parsed prefix.
+#
+# $1   - Prefix to parse
+setup_prefix() {
+  echo $1 | sed -e "s/<TARGET>/$TARGET/g" \
+                -e "s/<DATE>/$BUILD_DATE/g" \
+                -e "s/<CLIB>/$LIBC_MODE/g"
+}
+
+# Outputs one parsed prefix. Can take one or two parameters and decides
+# which one to parse.
+#
+# $1   - Default prefix to use if $2 missing
+# [$2] - Prefix to parse
+setup_prefix_default() {
+  if test "x$2" != "x" ; then
+    setup_prefix "$2"
+  else
+    setup_prefix "$1"
+  fi
+}
+
+# Output tokenized version
+#
+# $1 - Version string
+tokenize_version() {
+  # Replace '.' and '-' with space.
+  # Unify alpha,beta,pre,rc to lower case, make them separate tokens.
+  # Check that 'rc' is not part of longer word before tokenizing.
+  # Make numbers appearing after letters separate token.
+  echo $1 |
+    sed -e 's/\./ /g' -e 's/-/ /g' \
+        -e 's/[aA][lL][pP][hH][aA]/ alpha /g' \
+        -e 's/[bB][eE][tT][aA]/ beta /g' \
+        -e 's/[pP][rR][eE]/ pre /g' \
+        -e 's/[^alpha][rR][cC]/ rc /g' \
+        -e 's/\([^0-9]\)\([0-9]\)/\1 \2/g'
+}
+
+# Compare two version numbers
+#
+# $1 - First version
+# $2 - Second version
+#
+# 0  - Versions equal
+# 1  - $1 is greater than $2
+# 2  - $2 is greater than $1
+cmp_versions() {
+  VER1PARTS="$(tokenize_version $1)"
+  VER2PARTS="$(tokenize_version $2)"
+
+  for PART1 in $VER1PARTS
+  do
+    # First remaining part of VER2PARTS
+    PART2="$(echo $VER2PARTS | cut -f 1 -d ' ')"
+    if test "x$PART2" = "x"
+    then
+      if test "x$PART1" = "xalpha" || test "x$PART1" = "xbeta" ||
+         test "x$PART1" = "xpre"   || test "x$PART1" = "xrc"
+      then
+        # alpha and beta are less than pure version.
+        return 2
+      fi
+      return 1
+    fi
+    if test "x$PART1" != "x$PART2"
+    then
+      # Comparison between special version tokens.
+      # We consider increasing order (latter is newer version) to be:
+      #   alpha - beta - pre - rc
+      if test "x$PART1" = "xalpha"
+      then
+        return 2
+      fi
+      if test "x$PART2" = "xalpha"
+      then
+        return 1
+      fi
+      if test "x$PART1" = "xbeta"
+      then
+        return 2
+      fi
+      if test "x$PART2" = "xbeta"
+      then
+        return 1
+      fi
+      if test "x$PART1" = "xpre"
+      then
+        return 2
+      fi
+      if test "x$PART2" = "xpre"
+      then
+        return 1
+      fi
+      if test "x$PART1" = "xrc"
+      then
+        return 2
+      fi
+      if test "x$PART2" = "xrc"
+      then
+        return 1
+      fi
+      PART1NBR="$(echo $PART1 | sed 's/[^0-9].*//')"
+      PART2NBR="$(echo $PART2 | sed 's/[^0-9].*//')"
+      if test 0$PART1NBR -gt 0$PART2NBR
+      then
+        return 1
+      fi
+      if test 0$PART2NBR -gt 0$PART1NBR
+      then
+        return 2
+      fi
+      PART1TEXT="$(echo $PART1 | sed 's/[0-9]*//')"
+      PART2TEXT="$(echo $PART2 | sed 's/[0-9]*//')"
+      SMALLERTEXT="$(( echo $PART1TEXT && echo $PART2TEXT ) | sort | head -n 1)"
+      if test "x$PART2TEXT" = "x$SMALLERTEXT"
+      then
+        return 1
+      fi
+      return 2
+    fi
+    # Remove first, now handled, part of VER2PARTS
+    VER2PARTS="$(echo $VER2PARTS | sed 's/[^ ]*//')"
+  done
+
+  if test "x$VER2PARTS" != "x"
+  then
+    PART2=$(echo $VER2PARTS | cut -f 1 -d " ")
+    if test "x$PART2" = "xalpha" || test "x$PART2" = "xbeta" ||
+       test "x$PART2" = "xpre"   || test "x$PART2" = "xrc"
+    then
+      # alpha and beta are less than pure version.
+      return 1
+    fi
+    return 2
+  fi
+
+  return 0
+}
+
+# Check if version number is at least comparison version
+#
+# $1 - Version number
+# $2 - Comparison version
+is_minimum_version() {
+  cmp_versions "$1" "$2"
+
+  if test $? -eq 2
+  then
+    return 1
+  fi
+}
+
+# Check if version number is at most comparison version
+#
+# $1 - Version number
+# $2 - Comparison version
+is_max_version() {
+  cmp_versions "$1" "$2"
+
+  if test $? -eq 1
+  then
+    return 1
+  fi
+}
+
+# Check if version number is smaller than comparison version
+#
+# $1 - Version number
+# $2 - Comparison version
+is_smaller_version() {
+  cmp_versions "$1" "$2"
+
+  if test $? -ne 2
+  then
+    return 1
+  fi
+}
+
+# Check if version number is greater than comparison version
+#
+# $1 - Version number
+# $2 - Comparison version
+is_greater_version() {
+  cmp_versions "$1" "$2"
+
+  if test $? -ne 1
+  then
+    return 1
+  fi
+}
+
+# Recursively deletes directory
+#
+# $1 - Directory path
+#
+# 0  - Directory no longer exist
+# 1  - Directory still exist
+remove_dir() {
+  if ! test -d "$1"
+  then
+    # No such directory in the first place
+    return 0
+  fi
+
+  log_write 3 "Directory \"$1\" exist already - needs to be removed"
+
+  if test "x$FORCERM" = "xno"
+  then
+    return 1
+  fi
+
+  if test "x$FORCERM" != "xyes"
+  then
+    ANSWER="unknown"
+
+    while test "x$ANSWER" != "xyes" && test "x$ANSWER" != "xno"
+    do
+      echo "Directory \"$1\" already exist. Is it ok delete that directory and all its contents?"
+      echo "yes/no"
+      echo -n "> "
+      read ANSWER
+      case "x$ANSWER" in
+        xyes|xy|xYES|xYes) ANSWER="yes" ;;
+        xno|XNO|xNo) ANSWER="no" ;;
+        *) echo "Please answer \"yes\" or \"no\"." ;;
+      esac
+    done
+
+    if test "x$ANSWER" != "xyes"
+    then
+      return 1
+    fi
+  fi
+
+  if ! rm -Rf "$1"
+  then
+    return 1
+  fi
+}
