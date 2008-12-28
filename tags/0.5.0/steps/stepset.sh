e90ccbe72b530616d5bf9e6a533982e4b312c6e9
cazfi <cazfi@ed8cdf8a-1376-463e-89c1-a3058e460e0f>
2008-12-28 14:29:39 +0000

    Release 0.5.0

---
+++ tags/0.5.0/steps/stepset.sh
@@ -0,0 +1,27 @@
+# No shebang. This file must be sourced to actual script
+
+# stepfuncs.sh: Functions handling Crosser steps.
+#
+# (c) 2008 Marko Lindqvist
+#
+# This program is licensed under Gnu General Public License version 2.
+
+for _STEP in $STEPLIST
+do
+  case $_STEP in
+    native)
+      STEP_NATIVE=yes ;;
+    chain)
+      STEP_CHAIN=yes ;;
+    baselib)
+      STEP_BASELIB=yes ;;
+    win)
+      STEP_WIN=yes ;;
+    gtk)
+      STEP_GTK=yes ;;
+    sdl)
+      STEP_SDL=yes ;;
+    *)
+      log_error "Unknown step $_STEP in stepset.sh" ;;
+  esac
+done
