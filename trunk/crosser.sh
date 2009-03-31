0135216efba7cb0d32cdfad574c225da49eeaba4
cazfi <cazfi@ed8cdf8a-1376-463e-89c1-a3058e460e0f>
2009-03-31 14:58:58 +0000

    Do not try to run (native) strip

--- trunk/crosser.sh
+++ trunk/crosser.sh
@@ -897,6 +897,7 @@ then
      ! patch_src svgalib-$VERSION_SVGALIB svgalib_gentoo_k26            ||
      ! patch_src svgalib-$VERSION_SVGALIB svgalib_gentoo_k2628          ||
      ! patch_src svgalib-$VERSION_SVGALIB svgalib_arm_outsb             ||
+     ! patch_src svgalib-$VERSION_SVGALIB svgalib_nostrip               ||
      ! build_svgalib             svgalib svgalib-$VERSION_SVGALIB       \
         "clean install"                                                 ||
      ! unpack_component          SDL       $VERSION_SDL                 ||
