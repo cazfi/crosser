6900b8a79a4bafb34eb89452cad10e4f2c0bab80
cazfi <cazfi@ed8cdf8a-1376-463e-89c1-a3058e460e0f>
2011-06-27 16:48:17 +0000

    Created hack branch for creating working dllstacks until main tree has been properly fixed.

---
+++ branches/winfix/scripts/buildfuncs.sh
@@ -0,0 +1,226 @@
+# No shebang, this script is not executed, but sourced.
+
+# buildfuncs.sh: Build functions for Crosser
+#
+# (c) 2008-2010 Marko Lindqvist
+#
+# This program is licensed under Gnu General Public License version 2.
+
+#############################################################################
+
+# Generic function to compile one component
+#
+# $1   - Build directory in build hierarchy
+# $2   - Sourcedir in source hierarchy
+# $3   - Configure options
+# $4   - Make targets
+# [$5] - Build in srcdir ('yes')
+build_generic() {
+  log_packet "$1"
+
+  if test "x$5" != "xyes"
+  then
+    if test -d "$CROSSER_BUILDDIR/$1"
+    then
+      rm -Rf "$CROSSER_BUILDDIR/$1"
+    fi
+
+    if ! mkdir -p "$CROSSER_BUILDDIR/$1"
+    then
+       log_error "Failed to create directory \"$CROSSER_BUILDDIR/$1\""
+       return 1
+    fi
+    if ! cd "$CROSSER_BUILDDIR/$1"
+    then
+       log_error "Failed to change workdir to \"$CROSSER_BUILDDIR/$1\""
+       return 1
+    fi
+  else
+    if ! cd "$CROSSER_SRCDIR/$2"
+    then
+       log_error "Failed to change workdir to \"$CROSSER_SRCDIR/$2\""
+       return 1
+    fi
+  fi
+
+  if test -x "$CROSSER_SRCDIR/$2/configure"
+  then
+    log_write 1 "Configuring $2"
+    log_write 3 "  Options: \"$3\""
+    log_flags
+
+    if ! "$CROSSER_SRCDIR/$2/configure" $3 \
+        2>> "$CROSSER_LOGDIR/stderr.log" >> "$CROSSER_LOGDIR/stdout.log"
+    then
+      log_error "Configure failed: $1"
+      return 1
+    fi
+  fi
+
+  if test "x$4" != "x-" ; then
+    if test "x$4" != "x" ; then
+      MKTARGETS="$4"
+    else
+      MKTARGETS="all install"
+    fi
+
+    # We need old, dummy, libc.so for running configure for glibc itself,
+    # but it has to be removed before we make real one.
+    if test "x$1" = "xtgt-glibc" && test -e "$SYSPREFIX/usr/lib/libc.so"
+    then
+      log_write 3 "  Removing old libc.so"
+      rm -f "$SYSPREFIX/usr/lib/libc.so"
+    fi
+
+    if test "x$CROSSER_CORES" != "x"
+    then
+       COREOPT="-j $CROSSER_CORES"
+    fi
+
+    log_write 1 "Building $2"
+    log_write 3 "  Make targets: $MKTARGETS"
+    if ! make $COREOPT $MKTARGETS \
+        2>> "$CROSSER_LOGDIR/stderr.log" >> "$CROSSER_LOGDIR/stdout.log"
+    then
+      log_error "Make failed: $1"
+      return 1
+    fi
+  fi
+
+  # Reset hash in case it has old version of just built tool
+  hash -r
+
+  return 0
+}
+
+# Build with compiler built in native step
+#
+# $1 - Component to compile
+# $2 - Sourcedir in source hierarchy
+# $3 - Configure options
+# $4 - Make targets
+build_with_native_compiler() {
+  CONFOPTIONS="--build=$BUILD --host=$BUILD --target=$TARGET --prefix=$CROSSPREFIX $3 --disable-nls"
+
+  export CFLAGS="-O2"
+  export CPPFLAGS=""
+  export LDFLAGS=""
+
+  if ! build_generic "cross-$1" "$2" "$CONFOPTIONS" "$4"
+  then
+    return 1
+  fi
+}
+
+# Build component using cross-compiler
+#
+# $1   - Component name
+# $2   - Source dir in source hierarchy
+# $3   - Configure options
+# $4   - Make targets
+# [$5] - Destdir
+build_with_cross_compiler() {
+  if test "x$3" = "x"
+  then
+    PREFIXOPTION="--prefix=/usr"
+  else
+    PREFIXOPTION=""
+  fi
+  CONFOPTIONS="--build=$BUILD --host=$TARGET --target=$TARGET $PREFIXOPTION $3 --disable-nls"
+
+  export CFLAGS="-O2"
+  export CPPFLAGS="-isystem $SYSPREFIX/include -isystem $SYSPREFIX/usr/include"
+  export LDFLAGS="-L$CROSSER_IM_PFX/lib -L$SYSPREFIX/lib -L$SYSPREFIX/usr/lib"
+
+  if test "x$4" = "x"
+  then
+    MAKETARGETS="all install"
+  else
+    MAKETARGETS="$4"
+  fi
+
+  if test "x$5" = "x"
+  then
+    DESTDIR="$SYSPREFIX"
+  else
+    DESTDIR="$5"
+  fi
+
+  if ! build_generic "tgt-$1" "$2" "$CONFOPTIONS" "DESTDIR=$DESTDIR $MAKETARGETS"
+  then
+    return 1
+  fi
+}
+
+# Build component to native directory hierarchy
+#
+# $1   - Component name
+# $2   - Source dir in source hierarchy
+# $3   - Configure options
+# $4   - Make targets
+build_for_host() {
+  CONFOPTIONS="--build=$BUILD --host=$BUILD --target=$BUILD --prefix=$NATIVE_PREFIX $3"
+
+  export CFLAGS="-march=native -O2"
+  export CPPFLAGS=""
+  export LDFLAGS="-Wl,-rpath,$NATIVE_PREFIX/lib -L$NATIVE_PREFIX/lib"
+
+  if ! build_generic "host-$1" "$2" "$CONFOPTIONS" "$4"
+  then
+    return 1
+  fi
+}
+
+# Build svgalib using cross-compiler
+#
+# $1   - Component name
+# $2   - Source dir in source hierarchy
+# $3   - Make targets
+# [$4] - Destdir
+build_svgalib() {
+  export CFLAGS="-O2"
+  export CPPFLAGS="-isystem $SYSPREFIX/include -isystem $SYSPREFIX/usr/include"
+  export LDFLAGS="-L$SYSPREFIX/lib -L$SYSPREFIX/usr/lib"
+
+  export CC="$TARGET-gcc"
+
+  if test "x$3" = "x"
+  then
+    MAKETARGETS="clean install"
+  else
+    MAKETARGETS="$3"
+  fi
+
+  if test "x$4" = "x"
+  then
+    DESTDIR="$SYSPREFIX"
+  else
+    DESTDIR="$4"
+  fi
+
+  MFCFG="$CROSSER_SRCDIR/$2/Makefile.cfg"
+  sed -e "s,<TOPDIR>,$DESTDIR,g" \
+      -e "s,<KERNELVER>,$VERSION_KERNEL,g" \
+      -e "s,<ARCH>,$KERN_ARCH,g" \
+      "$MFCFG" > "$MFCFG.tmp"
+  mv "$MFCFG.tmp" "$MFCFG"
+  MFCFG="$CROSSER_SRCDIR/$2/kernel/svgalib_helper/Makefile"
+  sed -e "s,<TOPDIR>,$DESTDIR,g" \
+      -e "s,<KERNELVER>,$VERSION_KERNEL,g" \
+      -e "s,<ARCH>,$KERN_ARCH,g" \
+      -e "s,<TARGET>,$TARGET,g" \
+      "$MFCFG" > "$MFCFG.tmp"
+  mv "$MFCFG.tmp" "$MFCFG"
+
+  if test "x$CROSS_OFF" != "xyes"
+  then
+    SVGALIB_CROSS=yes
+  else
+    SVGALIB_CROSS=
+  fi
+
+  if ! CROSS_COMPILATION=$SVGALIB_CROSS build_generic "tgt-$1" "$2" "$CONFOPTIONS" "DESTDIR=$DESTDIR $MAKETARGETS" "yes"
+  then
+    return 1
+  fi
+}
---
+++ branches/winfix/scripts/download_packets.sh
@@ -0,0 +1,590 @@
+#!/bin/bash
+
+# download_packets.sh: Source package downloader
+#
+# (c) 2008-2011 Marko Lindqvist
+#
+# This program is licensed under Gnu General Public License version 2.
+
+
+# $1 - Base URL
+# $2 - Filename
+# $3 - Download directory
+download_file() {
+  if test "x$2" = "x" ; then
+    return 0
+  fi
+
+  if test "x$3" != "x"
+  then
+    DLDIR="$3"
+  else
+    DLDIR="."
+  fi
+
+  TIMEPART=$(date +%y%m%d%H%M)
+
+  if test -f filelist.txt
+  then
+    if grep ": $2\$" filelist.txt > /dev/null
+    then
+      sed "s/: .* : $2\$/: $TIMEPART : $2/" filelist.txt > filelist.tmp
+      mv filelist.tmp filelist.txt
+      APPEND=no
+    else
+      APPEND=yes
+    fi
+  else
+    APPEND=yes
+  fi
+
+  if test "x$APPEND" = "xyes"
+  then
+    echo "$TIMEPART : $TIMEPART : $2" >> filelist.txt
+  fi
+
+  if test -f "$DLDIR/$2"
+  then
+    echo "Already has $2, skipping"
+    return 0
+  fi
+
+  if ! ( cd "$DLDIR" && wget -T 180 -t 2 "$1$2" ) ; then
+    echo "Download of $2 failed" >&2
+    return 1
+  fi
+
+  echo "Downloaded $2"
+}
+
+# $1 - Base URL
+# $2 - Base name, usually base filename
+# $3 - Version      - or nonstandard filename (See $4)
+# $4 - Package type - empty means nonstandard naming (See $3)
+download_packet() {
+
+  if test "x$2" = "xgtk2" || test "x$2" = "xgtk3" ; then
+    BFNAME="gtk+"
+  else
+    BFNAME="$2"
+  fi
+
+  if test "x$4" = "xdsc" ; then
+    DLFILENAME="${BFNAME}_$3.$4"
+
+    if ! download_file "$1" "$DLFILENAME"
+    then
+      echo "Download of $BFNAME version $3 dsc file failed" >&2
+      return 1
+    fi
+
+    FILELIST_SECTION=no
+    cat "$DLDIR/$DLFILENAME" |
+    ( while read PART1 PART2 PART3
+      do
+        if test "x$FILELIST_SECTION" = "xyes"
+        then
+          if test "x$PART1" = "x"
+          then
+            FILELIST_SECTION=no
+          else
+            if ! download_file "$1" "$PART3"
+            then
+              echo "Download of $BFNAME version $3 file $PART3 failed" >&2
+              return 1
+            fi
+          fi
+        elif test "x$PART1" = "xFiles:"
+        then
+          FILELIST_SECTION=yes
+        fi
+      done
+    )
+  else
+    if test "x$4" = "x" ; then
+      DLFILENAME="$3"
+    else
+      DLFILENAME="$BFNAME-$3.$4"
+    fi
+    if ! download_file "$1" "$DLFILENAME"
+    then
+       echo "Download of $BFNAME version $3 failed" >&2
+       return 1
+    fi
+  fi
+}
+
+# $1 - Base URL
+# $2 - Packet name
+# $3 - Base filename
+# $4 - Number of patches
+download_patches_internal() {
+
+  declare -i DLNUM=1
+  declare -i DLTOTAL=$4
+
+  while test $DLNUM -le $DLTOTAL
+  do
+    if test $DLNUM -lt 10 ; then
+      ZEROES="00"
+    else
+      ZEROES="0"
+    fi
+    DLFILENAME="${3}${ZEROES}${DLNUM}"
+    if ! download_file "$1" "$DLFILENAME" "patch"
+    then
+      echo "Download of $2 patch $DLNUM failed" >&2
+      return 1
+    fi
+    DLNUM=$DLNUM+1
+  done
+}
+
+# $1 - Base URL
+# $2 - Base filename
+# $3 - Version
+# $4 - Package type
+# Return:
+# 0 - Downloaded
+# 1 - Failure
+# 2 - Not needed
+download_needed() {
+  if test "x$3" != "x"
+  then
+    PACKVER="$3"
+  else
+    PACKVER="$VERSION_SELECTED"
+  fi
+
+  if test "x$DOWNLOAD_PACKET" != "x" ; then
+    if test "x$DOWNLOAD_PACKET" = "x$2" ; then
+      download_packet "$1" "$2" "$PACKVER" "$4"
+      return $?
+    fi
+    return 2
+  fi
+  if test "x$STEPLIST" = "x" ; then
+    download_packet "$1" "$2" "$PACKVER" "$4"
+    return $?
+  fi
+  for STEP in $STEPLIST
+  do
+    BASENAME=$2
+    if belongs_to_step $BASENAME $STEP ; then
+      download_packet "$1" "$2" "$PACKVER" "$4"
+      return $?
+    fi
+  done
+
+  echo "$2 version $PACKVER not needed, skipping"
+  return 2
+}
+
+# $1 - Base URL
+# $2 - Packet name
+# $3 - Base filename
+# $4 - Version
+# $5 - Number of patches
+# Return:
+# 0 - Downloaded
+# 1 - Failure
+# 2 - Not needed
+download_patches() {
+  if test "x$4" != "x"
+  then
+    PACKVER="$4"
+  else
+    PACKVER="$VERSION_SELECTED"
+  fi
+
+  if test "x$DOWNLOAD_PACKET" != "x" ; then
+    if test "x$DOWNLOAD_PACKET" = "x$2" ; then
+      download_patches_internal "$1" "$2" "$3" "$5"
+      return $?
+    fi
+    return 2
+  fi
+  if test "x$STEPLIST" = "x" ; then
+    download_patches_internal "$1" "$2" "$3" "$5"
+    return $?
+  fi
+  for STEP in $STEPLIST
+  do
+    if belongs_to_step $2 $STEP ; then
+      download_patches_internal "$1" "$2" "$3" "$5"
+      return $?
+    fi
+  done
+
+  echo "$3 version $PACKVER patches not needed, skipping"
+  return 2
+}
+
+CROSSER_MAINDIR="$(cd "$(dirname "$0")/.." ; pwd)"
+
+if ! test -e "$CROSSER_MAINDIR/CrosserVersion"
+then
+  CROSSER_MAINDIR="/usr/share/crosser"
+fi
+
+if ! . "$CROSSER_MAINDIR/scripts/helpers.sh"
+then
+  echo "Failed to read $CROSSER_MAINDIR/scripts/helpers.sh" >&2
+  exit 1
+fi
+
+if test "x$1" = "x-h" || test "x$1" = "x--help" ; then
+  HELP_RETURN=0
+elif test "x$1" = "x" ; then
+  HELP_RETURN=1
+fi
+
+if test "x$HELP_RETURN" != "x" ; then
+  echo "Usage: $(basename "$0") <step> [versionset]"
+  echo "       $(basename "$0") --packet <name> [version] [patches]"
+  echo
+  echo " Possible steps:"
+  echo "  - native"
+  echo "  - chain"
+  echo "  - baselib"
+  echo "  - xorg"
+  echo "  - gtk"
+  echo "  - sdl"
+  echo "  - win"
+
+  exit $HELP_RETURN
+fi
+
+if test "x$1" = "x-v" || test "x$1" = "x--version"
+then
+  echo "Downloader for Crosser $CROSSER_VERSION"
+  exit 0
+fi
+
+if test "x$1" = "x--packet"
+then
+  if test "x$3" != "x"
+  then
+    export VERSION_SELECTED="$3"
+    if test "x$4" != "x" ; then
+      if test "x$2" != "xreadline"
+      then
+        echo "Number of patches given for component which has no patches." >&2
+        exit 1
+      fi
+      export PATCHES_SELECTED="$4"
+    else
+      export PATCHES_SELECTED="0"
+    fi
+  else
+    VERSIONSET="current"
+  fi
+elif test "x$1" != "x--packet" && test "x$2" != "x"
+then
+  VERSIONSET="$2"
+else
+  VERSIONSET="current"
+fi
+
+if test "x$VERSIONSET" != "x"
+then
+  if ! test -e "$CROSSER_MAINDIR/setups/$VERSIONSET.versions"
+  then
+    echo "Versionset $VERSIONSET.versions not found" >&2
+    exit 1
+  fi
+
+  if ! . "$CROSSER_MAINDIR/setups/$VERSIONSET.versions" ; then
+    echo "Failed to read list of package versions ($VERSIONSET.versions)" >&2
+    exit 1
+  fi
+fi
+
+if test -e "$CROSSER_MAINDIR/mirrors.conf" ; then
+  MIRRORCONF="$CROSSER_MAINDIR/mirrors.conf"
+elif test -e "$HOME/.crosser.mirrors" ; then
+  MIRRORCONF="$HOME/.crosser.mirrors"
+fi
+
+if test "x$MIRRORCONF" != "x" ; then
+  if ! . "$MIRRORCONF" ; then
+    echo "Problem in reading list of mirrors to use from $MIRRORCONF" >&2
+    exit 1
+  fi
+fi
+
+if ! . "$CROSSER_MAINDIR/steps/stepfuncs.sh" ; then
+  echo "Problem in reading stepfuncs.sh" >&2
+  exit 1
+fi
+
+if test "x$1" = "x--packet" ; then
+  if test "x$2" = "x" ; then
+    echo "No packet name given after --packet" >&2
+    exit 1
+  fi
+  DOWNLOAD_PACKET="$2"
+else
+  STEPLIST="$(parse_steplist $1)"
+  RET=$?
+  if test "x$RET" != "x0"
+  then
+    echo "Error in step parameters \"$1\"" >&2
+    exit 1
+  fi
+fi
+
+if test "x$MIRROR_GNU" = "x" ; then
+  MIRROR_GNU="ftp://ftp.gnu.org/gnu"
+fi
+
+if test "x$MIRROR_GCC" = "x" ; then
+  MIRROR_GCC="$MIRROR_GNU/gcc"
+fi
+
+if test "x$MIRROR_KERNEL" = "x" ; then
+  MIRROR_KERNEL="http://www.all.kernel.org"
+fi
+
+if test "x$MIRROR_SOURCEWARE" = "x" ; then
+  MIRROR_SOURCEWARE="ftp://sources.redhat.com"
+fi
+
+if test "x$MIRROR_DEB" = "x" ; then
+  MIRROR_DEB="http://ftp.debian.org/debian"
+fi
+
+if test "x$MIRROR_CUPS" = "x" ; then
+  MIRROR_CUPS="http://ftp.easysw.com/pub"
+fi
+
+if test "x$MIRROR_XORG" = "x" ; then
+  MIRROR_XORG="http://xorg.freedesktop.org/releases"
+fi
+
+MIRROR_SOURCEFORGE="http://downloads.sourceforge.net"
+MIRROR_GNOME="http://ftp.gnome.org/pub/gnome"
+MIRROR_SAVANNAH="http://download.savannah.gnu.org"
+
+if test "x$VERSION_SELECTED" != "x"
+then
+  case $DOWNLOAD_PACKET in
+    bzip2)       VERSION_BZIP2=$VERSION_SELECTED ;;
+    glib)        VERSION_GLIB=$VERSION_SELECTED ;;
+    pango)       VERSION_PANGO=$VERSION_SELECTED ;;
+    gdk-pixbuf)  VERSION_GDK_PIXBUF=$VERSION_SELECTED ;;
+    gtk2)        VERSION_GTK2=$VERSION_SELECTED ;;
+    gtk3)        VERSION_GTK3=$VERSION_SELECTED ;;
+    gtk-engines) VERSION_GTK_ENG=$VERSION_SELECTED ;;
+    gtk-doc)     VERSION_GTK_DOC=$VERSION_SELECTED ;;
+    atk)         VERSION_ATK=$VERSION_SELECTED ;;
+    gcc)         VERSION_GCC=$VERSION_SELECTED ;;
+    cups)        VERSION_CUPS=$VERSION_SELECTED ;;
+    readline)    VERSION_READLINE=$VERSION_SELECTED
+                 PATCHES_READLINE=$PATCHES_SELECTED ;;
+    automake)    VERSION_AUTOMAKE=$VERSION_SELECTED ;;
+    libtool)     VERSION_LIBTOOL=$VERSION_SELECTED ;;
+    mpfr)        VERSION_MPFR=$VERSION_SELECTED ;;
+    Python)      VERSION_PYTHON=$VERSION_SELECTED ;;
+    jpeg)        VERSION_JPEG=$VERSION_SELECTED ;;
+    xproto)      VERSION_XORG_XPROTO=$VERSION_SELECTED ;;
+    xextproto)   VERSION_XORG_XEXTPROTO=$VERSION_SELECTED ;;
+    xtrans)      VERSION_XORG_XTRANS=$VERSION_SELECTED ;;
+    libXau)      VERSION_XORG_LIBXAU=$VERSION_SELECTED ;;
+    libX11)      VERSION_XORG_LIBX11=$VERSION_SELECTED ;;
+  esac
+fi
+GLIB_DIR="$(echo $VERSION_GLIB | sed 's/\./ /g' | (read MAJOR MINOR PATCH ; echo -n $MAJOR.$MINOR ))"
+PANGO_DIR="$(echo $VERSION_PANGO | sed 's/\./ /g' | (read MAJOR MINOR PATCH ; echo -n $MAJOR.$MINOR ))"
+GDK_PB_DIR="$(echo $VERSION_GDK_PIXBUF | sed 's/\./ /g' | (read MAJOR MINOR PATCH ; echo -n $MAJOR.$MINOR ))"
+GTK2_DIR="$(echo $VERSION_GTK2 | sed 's/\./ /g' | (read MAJOR MINOR PATCH ; echo -n $MAJOR.$MINOR ))"
+GTK3_DIR="$(echo $VERSION_GTK3 | sed 's/\./ /g' | (read MAJOR MINOR PATCH ; echo -n $MAJOR.$MINOR ))"
+GTK_ENG_DIR="$(echo $VERSION_GTK_ENG | sed 's/\./ /g' | (read MAJOR MINOR PATCH ; echo -n $MAJOR.$MINOR ))"
+GTK_DOC_DIR="$(echo $VERSION_GTK_DOC | sed 's/\./ /g' | (read MAJOR MINOR ; echo -n $MAJOR.$MINOR ))"
+ATK_DIR="$(echo $VERSION_ATK | sed 's/\./ /g' | (read MAJOR MINOR PATCH ; echo -n $MAJOR.$MINOR ))"
+
+READLINE_SHORT="$(echo $VERSION_READLINE | sed 's/\.//g')"
+
+case "x$VERSION_XORG_XPROTO" in
+  x) ;;
+  x7.0.13) VERSION_XORG=X11R7.4 ;;
+  x7.0.16) VERSION_XORG=X11R7.5 ;;
+  x7.0.20) VERSION_XORG=X11R7.6 ;;
+esac
+
+case "x$VERSION_XORG_XEXTPROTO" in
+  x) ;;
+  x7.0.3) VERSION_XORG=X11R7.4 ;;
+  x7.1.1) VERSION_XORG=X11R7.5 ;;
+  x7.1.2) VERSION_XORG=X11R7.6 ;;
+esac
+
+case "x$VERSION_XORG_XTRANS" in
+  x) ;;
+  x1.2.1) VERSION_XORG=X11R7.4 ;;
+  x1.2.5) VERSION_XORG=X11R7.5 ;;
+  x1.2.6) VERSION_XORG=X11R7.6 ;;
+esac
+
+case "x$VERSION_XORG_LIBXAU" in
+  x) ;;
+  x1.0.4) VERSION_XORG=X11R7.4 ;;
+  x1.0.5) VERSION_XORG=X11R7.5 ;;
+  x1.0.6) VERSION_XORG=X11R7.6 ;;
+esac
+
+case "x$VERSION_XORG_LIBX11" in
+  x) ;;
+  x1.1.5) VERSION_XORG=X11R7.4 ;;
+  x1.3.2) VERSION_XORG=X11R7.5 ;;
+  x1.4.0) VERSION_XORG=X11R7.6 ;;
+esac
+
+if is_minimum_version $VERSION_AUTOMAKE 1.6.1
+then
+  AUTOMAKE_PACK="tar.bz2"
+else
+  AUTOMAKE_PACK="tar.gz"
+fi
+
+if is_minimum_version $VERSION_LIBTOOL 2.2.6a
+then
+  LIBTOOL_PACK="tar.gz"
+else
+  LIBTOOL_PACK="tar.bz2"
+fi
+
+download_needed "$MIRROR_GNU/libtool/"  "libtool"  "$VERSION_LIBTOOL"  $LIBTOOL_PACK
+
+RET="$?"  
+download_needed "$MIRROR_GNU/binutils/" "binutils" "$VERSION_BINUTILS" "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_GNU/gawk/"     "gawk"     "$VERSION_GAWK"     "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_GNU/autoconf/" "autoconf" "$VERSION_AUTOCONF" "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_GNU/automake/" "automake" "$VERSION_AUTOMAKE" "$AUTOMAKE_PACK"
+RET="$RET $?"
+download_needed "http://www.python.org/ftp/python/$VERSION_PYTHON/" "Python" "$VERSION_PYTHON" "tgz"
+RET="$RET $?"
+download_needed "http://pkgconfig.freedesktop.org/releases/" "pkg-config" "$VERSION_PKG_CONFIG" "tar.gz"
+RET="$RET $?"
+download_needed "$MIRROR_GCC/gcc-$VERSION_GCC/" "gcc" "$VERSION_GCC" "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_GNU/glibc/"    "glibc"    "$VERSION_GLIBC"    "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_GNU/glibc/"    "glibc-ports" "$VERSION_GLIBC_PORTS" "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_DEB/pool/main/e/eglibc/"       "eglibc"     "$VERSION_EGLIBC_DEB" "dsc"
+RET="$RET $?"
+download_needed "$MIRROR_KERNEL/pub/linux/kernel/v2.6/" "linux" "$VERSION_KERNEL"  "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_SOURCEWARE/pub/newlib/"        "newlib" "$VERSION_NEWLIB" "tar.gz"
+RET="$RET $?"
+download_needed "http://ftp.sunet.se/pub/gnu/gmp/"      "gmp"    "$VERSION_GMP"    "tar.bz2"
+RET="$RET $?"
+download_needed "http://www.mpfr.org/mpfr-$VERSION_MPFR/" "mpfr"   "$VERSION_MPFR"   "tar.bz2"
+RET="$RET $?"
+download_needed "http://www.multiprecision.org/mpc/download/" "mpc" "$VERSION_MPC"   "tar.gz"
+RET="$RET $?"
+download_needed "$MIRROR_GNU/libiconv/"                 "libiconv"   "$VERSION_ICONV"      "tar.gz"
+RET="$RET $?"
+download_needed "$MIRROR_SOURCEFORGE/libpng/"           "libpng"     "$VERSION_PNG"        "tar.bz2"
+RET="$RET $?"
+download_needed "http://my.arava.co.il/~matan/svgalib/" "svgalib"    "$VERSION_SVGALIB"   "tar.gz"
+RET="$RET $?"
+download_needed "$MIRROR_DEB/pool/main/z/zlib/"         "zlib"       "$VERSION_ZLIB"       "dsc"
+RET="$RET $?"
+download_needed "http://www.bzip.org/$VERSION_BZIP2/"   "bzip2"      "$VERSION_BZIP2"      "tar.gz"
+RET="$RET $?"
+download_needed "$MIRROR_GNU/readline/"                 "readline"   "$VERSION_READLINE"   "tar.gz"
+RET="$RET $?"
+download_patches "$MIRROR_GNU/readline/readline-$VERSION_READLINE-patches/" \
+                 "readline"            "readline${READLINE_SHORT}-" \
+                 "$VERSION_READLINE"   "$PATCHES_READLINE"
+RET="$RET $?"
+download_needed "$MIRROR_GNU/gettext/"                  "gettext"    "$VERSION_GETTEXT"    "tar.gz"
+RET="$RET $?"
+download_needed "$MIRROR_GNOME/sources/glib/$GLIB_DIR/" "glib"       "$VERSION_GLIB"       "tar.bz2"
+RET="$RET $?"
+download_needed "http://www.ijg.org/files/"             "jpeg"       "jpegsrc.v${VERSION_JPEG}.tar.gz"
+RET="$RET $?"
+download_needed "ftp://ftp.remotesensing.org/pub/libtiff/" "tiff"    "$VERSION_TIFF"       "tar.gz"
+RET="$RET $?"
+download_needed "$MIRROR_SOURCEFORGE/expat/"            "expat"      "$VERSION_EXPAT"      "tar.gz"
+RET="$RET $?"
+download_needed "$MIRROR_SAVANNAH/releases/freetype/"   "freetype"   "$VERSION_FREETYPE"   "tar.bz2"
+RET="$RET $?"
+download_needed "http://fontconfig.org/release/"        "fontconfig" "$VERSION_FONTCONFIG" "tar.gz"
+RET="$RET $?"
+download_needed "http://curl.haxx.se/download/"         "curl"       "$VERSION_CULR" "tar.bz2"
+RET="$RET $?"
+download_needed "http://cairographics.org/releases/"    "pixman"     "$VERSION_PIXMAN"     "tar.gz"
+RET="$RET $?"
+download_needed "http://cairographics.org/releases/"    "cairo"      "$VERSION_CAIRO"      "tar.gz"
+RET="$RET $?"
+download_needed "$MIRROR_GNOME/sources/pango/$PANGO_DIR/" "pango"    "$VERSION_PANGO"      "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_GNOME/sources/atk/$ATK_DIR/"   "atk"        "$VERSION_ATK"        "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_GNOME/sources/gtk-doc/$GTK_DOC_DIR/" "gtk-doc" "$VERSION_GTK_DOC" "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_GNOME/sources/gdk-pixbuf/$GDK_PB_DIR/" "gdk-pixbuf" "$VERSION_GDK_PIXBUF"  "tar.bz2" 
+RET="$RET $?"
+download_needed "$MIRROR_GNOME/sources/gtk+/$GTK2_DIR/" "gtk2"       "$VERSION_GTK2"        "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_GNOME/sources/gtk+/$GTK3_DIR/" "gtk3"       "$VERSION_GTK3"        "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_GNOME/sources/gtk-engines/$GTK_ENG_DIR/"  "gtk-engines" "$VERSION_GTK_ENG" "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_CUPS/cups/$VERSION_CUPS/"     "cups"       "cups-$VERSION_CUPS-source.tar.bz2"
+RET="$RET $?"
+download_needed "http://www.libsdl.org/release/"        "SDL"        "$VERSION_SDL"        "tar.gz"
+RET="$RET $?"
+download_needed "http://www.libsdl.org/projects/SDL_image/release/" "SDL_image"  "$VERSION_SDL_IMAGE"  "tar.gz"
+RET="$RET $?"
+download_needed "http://www.libsdl.org/projects/SDL_mixer/release/" "SDL_mixer"  "$VERSION_SDL_MIXER"  "tar.gz"
+RET="$RET $?"
+download_needed "ftp://xmlsoft.org/libxml2/"                 "libxml2"   "$VERSION_LIBXML2"        "tar.gz"
+RET="$RET $?"
+download_needed "ftp://xmlsoft.org/libxslt/"                 "libxslt"   "$VERSION_LIBXSLT"        "tar.gz"
+RET="$RET $?"
+download_needed "http://xcb.freedesktop.org/dist/"           "libpthread-stubs" "$VERSION_PTHREAD_STUBS" "tar.bz2"
+RET="$RET $?"
+download_needed "http://xcb.freedesktop.org/dist/"           "xcb-proto" "$VERSION_XCB_PROTO"      "tar.bz2"
+RET="$RET $?"
+download_needed "http://xcb.freedesktop.org/dist/"           "libxcb"    "$VERSION_LIBXCB"         "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_XORG/$VERSION_XORG/src/everything/" "xproto"    "$VERSION_XORG_XPROTO"    "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_XORG/$VERSION_XORG/src/everything/" "xextproto" "$VERSION_XORG_XEXTPROTO" "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_XORG/$VERSION_XORG/src/everything/" "xtrans"    "$VERSION_XORG_XTRANS" "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_XORG/$VERSION_XORG/src/everything/" "libXau"    "$VERSION_XORG_LIBXAU" "tar.bz2"
+RET="$RET $?"
+download_needed "$MIRROR_XORG/$VERSION_XORG/src/everything/" "libX11"    "$VERSION_XORG_LIBX11" "tar.bz2"
+RET="$RET $?"
+download_needed "ftp://ftp.gnupg.org/gcrypt/libgpg-error/"   "libgpg-error" "$VERSION_GPGERROR" "tar.bz2"
+RET="$RET $?"
+download_needed "ftp://ftp.gnupg.org/gcrypt/libgcrypt/"      "libgcrypt" "$VERSION_LIBGCRYPT"   "tar.bz2"
+RET="$RET $?"
+
+for VALUE in $RET
+do
+  if test "$VALUE" = "0" ; then
+    DOWNLOADED=true
+  elif test "$VALUE" = "1" ; then
+    FAILED=true
+  fi
+done
+
+if test "x$DOWNLOAD_PACKET" != "x" && test "x$DOWNLOADED" != "xtrue" ; then
+  echo "Download instructions for $DOWNLOAD_PACKET not found." >&2
+  exit 1
+fi
+
+if test "x$FAILED" = "xtrue" ; then
+  echo "Some packet(s) failed to download." >&2
+  exit 1
+fi
