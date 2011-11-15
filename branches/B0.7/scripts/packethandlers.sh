3e5ef3c2ede5aaeab992c0f926416eef462f0e51
cazfi <cazfi@ed8cdf8a-1376-463e-89c1-a3058e460e0f>
2011-11-15 06:19:49 +0000

    Fixed build with binutils version 2.21.1a

--- branches/B0.7/scripts/packethandlers.sh
+++ branches/B0.7/scripts/packethandlers.sh
@@ -2,7 +2,7 @@
 
 # packethandlers.sh: Functions for Crosser
 #
-# (c) 2008-2009 Marko Lindqvist
+# (c) 2008-2011 Marko Lindqvist
 #
 # This program is licensed under Gnu General Public License version 2.
 
@@ -40,3 +40,11 @@ basever_libtool() {
     echo $1
   fi
 }
+
+# Echo base version of binutils version string
+#
+# $1 - Full version
+basever_binutils() {
+  echo $1 | sed 's/[a-z]//g'
+}
+
