548c1bc0a8cc5c6e6b9703eadf13fc884cc1ba09
Marko Lindqvist <cazfi74@gmail.com>
2011-11-16 15:08:55 +0000

    Updated atk to version 2.2.0

--- dllstack.sh
+++ dllstack.sh
@@ -629,6 +629,7 @@ if ! unpack_component  fontconfig $VERSION_FONTCONFIG               ||
    ! free_component    pango      $VERSION_PANGO "pango"          ||
    ! unpack_component  atk        $VERSION_ATK                    ||
    ! ( is_smaller_version $VERSION_ATK     1.24.0  ||
+       is_minimum_version $VERSION_ATK     2.2.0   ||
        patch_src          atk-$VERSION_ATK atk_def    )           ||
    ! autogen_component atk        $VERSION_ATK                    \
      "libtoolize aclocal automake autoconf"                       ||
@@ -661,7 +662,7 @@ if ! ( is_smaller_version $VERSION_GTK2 2.22.0 ||
    ! free_component   gtk+        $VERSION_GTK2 "gtk2"            ||
    ! unpack_component gtk3        $VERSION_GTK3                   ||
    ! rm -f $CROSSER_SRCDIR/gtk+-$VERSION_GTK3/gdk/gdkconfig.h     ||
-   ! ( is_smaller_version "$VERSION_GTK3" 3.0.0 ||
+   ! ( is_minimum_version "$VERSION_GTK3" 3.2.0 ||
        patch_src gtk+-$VERSION_GTK3 gtk3_marshalers )             ||
    ! build_component_full gtk3 gtk+ $VERSION_GTK3                 ||
    ! free_component   gtk+        $VERSION_GTK3 "gtk3"            ||
