2789833a44d3539ca7fd85f9c11929f1350aea22
Marko Lindqvist <cazfi74@gmail.com>
2012-12-27 11:05:54 +0000

    Updated gettext to version 0.18.2

--- dllstack.sh
+++ dllstack.sh
@@ -489,12 +489,13 @@ if ! build_component   libtool    $BASEVER_LIBTOOL                   ||
    ! build_component   libpng     $VERSION_PNG                       ||
    ! free_component    libpng     $VERSION_PNG "libpng"              ||
    ! unpack_component  gettext    $VERSION_GETTEXT                   ||
-   ! ( patch_src gettext-$VERSION_GETTEXT gettext_cxx_tools &&
-       ( cd "$CROSSER_SRCDIR/gettext-$VERSION_GETTEXT" &&
-         libtoolize &&
-         ./autogen.sh --quick --skip-gnulib ) \
-         >> "$CROSSER_LOGDIR/stdout.log" 2>> "$CROSSER_LOGDIR/stderr.log"
-     )                                                               ||
+   ! ( is_minimum_version $VERSION_GETTEXT 0.18.2 ||
+       ( patch_src gettext-$VERSION_GETTEXT gettext_cxx_tools &&
+         ( cd "$CROSSER_SRCDIR/gettext-$VERSION_GETTEXT" &&
+           libtoolize &&
+           ./autogen.sh --quick --skip-gnulib ) \
+           >> "$CROSSER_LOGDIR/stdout.log" 2>> "$CROSSER_LOGDIR/stderr.log"
+     ))                                                               ||
    ! (export LIBS="-liconv" && build_component gettext  $VERSION_GETTEXT \
                                "$GETTEXT_VARS --enable-relocatable" ) ||
    ! free_component    gettext    $VERSION_GETTEXT "gettext"          ||
