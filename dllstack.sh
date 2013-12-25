747cc5a63102e91143021f9ea34bc20e3736f113
Marko Lindqvist <cazfi74@gmail.com>
2013-12-25 11:34:29 +0000

    Added qt_sharappidinfolink.patch

--- dllstack.sh
+++ dllstack.sh
@@ -737,13 +737,14 @@ fi
 
 if test "x$BUILD_QT" = "xyes"
 then
-if ! unpack_component qt-everywhere-opensource-src $VERSION_QT             ||
-   ! patch_src qt-everywhere-opensource-src $VERSION_QT "qt_pkgconfig"     ||
-   ! patch_src qt-everywhere-opensource-src $VERSION_QT "qt_freetype_libs" ||
-   ! build_component_full  qt-everywhere-opensource-src                    \
-     qt-everywhere-opensource-src $VERSION_QT                              \
+if ! unpack_component qt-everywhere-opensource-src $VERSION_QT                 ||
+   ! patch_src qt-everywhere-opensource-src $VERSION_QT "qt_pkgconfig"         ||
+   ! patch_src qt-everywhere-opensource-src $VERSION_QT "qt_freetype_libs"     ||
+   ! patch_src qt-everywhere-opensource-src $VERSION_QT "qt_sharappidinfolink" ||
+   ! build_component_full  qt-everywhere-opensource-src                        \
+     qt-everywhere-opensource-src $VERSION_QT                                  \
      "-opensource -confirm-license -xplatform win32-g++ -device-option CROSS_COMPILE=${TARGET}- -system-zlib -nomake examples -force-pkg-config -no-gtkstyle" \
-     "qt" "" "no"                                                          ||
+     "qt" "" "no"                                                              ||
    ! free_component   qt-everywhere-opensource-src $VERSION_QT "qt-everywhere-opensource-src"
 then
   log_error "QT stack build failed"
