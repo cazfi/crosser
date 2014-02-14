f873e33ed1893713532c7eeeb2dc5762e9d48b84
cazfi <cazfi@ed8cdf8a-1376-463e-89c1-a3058e460e0f>
2014-02-14 00:14:32 +0000

    Added mpfr to chain build

--- trunk/scripts/download_packets.sh
+++ trunk/scripts/download_packets.sh
@@ -512,6 +512,8 @@ download_needed "http://pkgconfig.freedesktop.org/releases/" "pkg-config" "$VERS
 RET="$RET $?"
 download_needed "$MIRROR_GNU/gmp/"      "gmp"      "$VERSION_GMP"      "tar.xz"
 RET="$RET $?"
+download_needed "http://www.mpfr.org/mpfr-current/"     "mpfr"       "$VERSION_MPFR"       "tar.xz"
+RET="$RET $?"
 download_needed "$MIRROR_GNU/libiconv/"                 "libiconv"   "$VERSION_ICONV"      "tar.gz"
 RET="$RET $?"
 # download_needed "$MIRROR_SOURCEFORGE/projects/libpng/files/$PNG_DIR/older-releases/$VERSION_PNG/" "libpng" "$VERSION_PNG" "$PNG_PACK"
