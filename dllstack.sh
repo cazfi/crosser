19a39655e0b03382a7a55dbd6bf4ea86efa51eed
Marko Lindqvist <cazfi74@gmail.com>
2014-07-04 02:05:28 +0000

    Fixed build with fontconfig 2.11.1

--- dllstack.sh
+++ dllstack.sh
@@ -605,7 +605,7 @@ if ! unpack_component tiff                                                  ||
        patch_src fontconfig $VERSION_FONTCONFIG fontconfig_buildsys_flags)  ||
    ! ( is_smaller_version $VERSION_FONTCONFIG 2.10 ||
        patch_src fontconfig $VERSION_FONTCONFIG fontconfig_cross )          ||
-   ! ( is_smaller_version $VERSION_FONTCONFIG 2.11 ||
+   ! ( ! cmp_versions $VERSION_FONTCONFIG 2.11 ||
        patch_src fontconfig $VERSION_FONTCONFIG fontconfig_disable_test )   ||
    ! autogen_component fontconfig $VERSION_FONTCONFIG                       \
       "libtoolize aclocal automake autoconf"                                ||
