96ca9bf09b9b85c389df5b706d952635c3c80c8f
cazfi <cazfi@ed8cdf8a-1376-463e-89c1-a3058e460e0f>
2015-03-05 19:11:33 +0000

    Set -static-libgcc and -static-libstdc++ to QMAKE_LFLAGS

--- trunk/dllstack.sh
+++ trunk/dllstack.sh
@@ -816,6 +816,7 @@ if ! unpack_component qt-everywhere-opensource-src                              
    ! patch_src qt-everywhere-opensource-src $VERSION_QT "qt_sharappidinfolink"  ||
    ! patch_src qt-everywhere-opensource-src $VERSION_QT "qt_g++"                ||
    ! patch_src qt-everywhere-opensource-src $VERSION_QT "qt_disableidc"         ||
+   ! patch_src qt-everywhere-opensource-src $VERSION_QT "qt_linkflags"          ||
    ! build_component_full  qt-everywhere-opensource-src                         \
      qt-everywhere-opensource-src                                               \
      "-opensource -confirm-license -xplatform win32-g++ -device-option CROSS_COMPILE=${TARGET}- -system-zlib -nomake examples -force-pkg-config -no-gtkstyle -no-opengl" \
