b372bf322021ca9187b05ff070d6b1f30b32d7cd
cazfi <cazfi@ed8cdf8a-1376-463e-89c1-a3058e460e0f>
2015-03-25 17:29:52 +0000

    Updated:  * glib -> 2.44.0  * harfbuzz -> 0.9.40  * atk -> 2.16.0  * adwaita-icon-theme -> 3.16.0  * gnome-themes-standard -> 3.16.0

--- trunk/dllstack.sh
+++ trunk/dllstack.sh
@@ -697,6 +697,7 @@ if ! build_component  gdk-pixbuf                                      ||
        ( patch_src gtk+ $VERSION_GTK3 gtk3_nogdkdef &&
          patch_src gtk+ $VERSION_GTK3 gtk3_nogtkdef ))                ||
    ! ( is_smaller_version $VERSION_GTK3 3.14.0 ||
+       is_minimum_version $VERSION_GTK3 3.16.0 ||
        patch_src gtk+ $VERSION_GTK3 gtk3_extstring_cross)             ||
    ! (! cmp_versions $VERSION_GTK3 3.14.5 ||
       patch_src gtk+ $VERSION_GTK3 gtk3_noplug )                      ||
