3a068462eb210ed19af0441753e41bea1501c971
cazfi <cazfi@ed8cdf8a-1376-463e-89c1-a3058e460e0f>
2015-07-03 21:02:45 +0000

    Truly obsolete PREFIX configuration variable

--- trunk/scripts/setup_reader.sh
+++ trunk/scripts/setup_reader.sh
@@ -87,10 +87,6 @@ if test "x$DLLSPREFIX" = "x" && test "x$CROSSER_DST_PFX" != "x" ; then
   echo "Configuration variable CROSSER_DST_PFX is deprecated. Please use DLLSPREFIX." >&2
   DLLSPREFIX="$CROSSER_DST_PFX"
 fi
-if test "x$DLLSPREFIX" = "x" && test "x$PREFIX" != "x" ; then
-  echo "Configuration variable PREFIX is deprecated. Please use DLLSPREFIX." >&2
-  DLLSPREFIX="$PREFIX"
-fi
 if test "x$CROSSER_QT" = "x" ; then
     CROSSER_QT="no"
 fi
