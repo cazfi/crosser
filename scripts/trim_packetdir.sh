3a488fd2d70e952f5e434de7bef32c842a90be5e
Marko Lindqvist <cazfi74@gmail.com>
2015-10-11 19:20:32 +0000

    trim_packetdir.sh removes all nonexisting files from the filelist

--- scripts/trim_packetdir.sh
+++ scripts/trim_packetdir.sh
@@ -2,7 +2,7 @@
 
 # trim_packetdir.sh: Delete old source packets
 #
-# (c) 2009-2012 Marko Lindqvist
+# (c) 2009-2015 Marko Lindqvist
 #
 # This program is licensed under Gnu General Public License version 2.
 
@@ -53,9 +53,20 @@ if ! test -f "$1/filelist.txt" ; then
   exit 1
 fi
 
+# First clean list from files that do not exist
+( cat "$1/filelist.txt" | while read F1 F2 F3 F4 F5
+  do
+    if ! test -e "$1/$F5" ; then
+      echo "Removing entry \"$1/$F5\" from filelist as it doesn't exist" >&2
+    else
+      echo $F1 $F2 $F3 $F4 $F5
+    fi
+  done
+) > "$1/filelist.tmp"
+
 declare -i FILECOUNT=$(wc -l "$1/filelist.txt" | sed 's/ .*//')
 
-( sort -k 3 "$1/filelist.txt" | while read F1 F2 F3 F4 F5
+( sort -k 3 "$1/filelist.tmp" | while read F1 F2 F3 F4 F5
   do
     if test $FILECOUNT -gt $FILELIMIT ; then
       rm "$1/$F5"
@@ -64,6 +75,4 @@ declare -i FILECOUNT=$(wc -l "$1/filelist.txt" | sed 's/ .*//')
       echo $F1 $F2 $F3 $F4 $F5
     fi
   done
-) > "$1/filelist.tmp"
-
-mv "$1/filelist.tmp" "$1/filelist.txt"
+) > "$1/filelist.txt"
