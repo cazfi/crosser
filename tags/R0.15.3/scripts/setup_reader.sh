d2723d1d609e725e57de89f5b6d0536924459db6
cazfi <cazfi@ed8cdf8a-1376-463e-89c1-a3058e460e0f>
2015-10-13 04:15:44 +0000

    Release 0.15.3

---
+++ tags/R0.15.3/scripts/setup_reader.sh
@@ -0,0 +1,100 @@
+# No shebang, this script is not executed, but sourced.
+
+# setup_reader.sh: Setup build environment variables
+#
+# (c) 2008-2014 Marko Lindqvist
+#
+# This program is licensed under Gnu General Public License version 2.
+
+if test "x$CROSSER_TMPDIR" = "x"
+then
+  CROSSER_TMPDIR="/tmp/crosser-$(whoami)"
+fi
+
+if test "x$CROSSER_GLOBAL_CONF" = "x" && test -e "/etc/crosser.conf" ; then
+  CROSSER_GLOBAL_CONF="/etc/crosser.conf"
+fi
+
+if test "x$CROSSER_GLOBAL_CONF" != "x" ; then
+  if ! test -e "$CROSSER_GLOBAL_CONF" ; then
+    echo "Error: Can't find global configuration file \"$CROSSER_GLOBAL_CONF\"" >&2
+    exit 1
+  fi
+  . "$CROSSER_GLOBAL_CONF"
+fi
+
+if test "x$CROSSER_CONF" != "x" ; then
+  if test -e "$CROSSER_CONF" ; then
+    . "$CROSSER_CONF"
+  else
+    echo "Error: Can't find specified configuration file \"$CROSSER_CONF\"" >&2
+    exit 1
+  fi
+elif test -e $CROSSER_MAINDIR/local_setup.conf ; then
+  . $CROSSER_MAINDIR/local_setup.conf
+elif test -e $HOME/.crosser.conf ; then
+  . $HOME/.crosser.conf
+elif test "x$CROSSER_GLOBAL_CONF" = "x" ; then
+  echo "Warning: No configuration found. Trying to build with default values." >&2
+  echo "         Read doc/setup.txt for configuration instructions." >&2
+fi
+
+if test "x$PACKETDIR" = "x" ; then
+  PACKETDIR="$HOME/.crosser/packets"
+fi
+if test "x$CROSSER_BUILDDIR" = "x" && test "x$MAINBUILDDIR" != "x"
+then
+  echo "Configuration variable MAINBUILDDIR is deprecated. Please use CROSSER_BUILDDIR" >&2
+  CROSSER_BUILDDIR="$MAINBUILDDIR"
+fi
+if test "x$CROSSER_BUILDDIR" = "x" ; then
+  CROSSER_BUILDDIR="$CROSSER_TMPDIR/build"
+fi
+if test "x$CROSSER_SRCDIR" = "x" && test "x$MAINSRCDIR" != "x"
+then
+  echo "Configuration variable MAINSRCDIR is deprecated. Please use CROSSER_SRCDIR" >&2
+  CROSSER_SRCDIR="$MAINSRCDIR"
+fi
+if test "x$CROSSER_SRCDIR" = "x" ; then
+  CROSSER_SRCDIR="$CROSSER_TMPDIR/src"
+fi
+if test "x$CROSSER_LOGDIR" = "x" && test "x$LOGDIR" != "x"
+then
+  echo "Configuration variable LOGDIR is deprecated. Please use CROSSER_LOGDIR" >&2
+  CROSSER_LOGDIR="$LOGDIR"
+fi
+if test "x$CROSSER_LOGDIR" = "x" ; then
+  CROSSER_LOGDIR="$HOME/.crosser/log"
+fi
+if test "x$CROSSER_DOWNLOAD" = "x" ; then
+  CROSSER_DOWNLOAD="demand"
+fi
+if test "x$CROSSER_CORES" != "x" ; then
+  declare -i CTMP="$CROSSER_CORES"
+  if test $CTMP -lt 1 ; then
+    echo "Illegal CROSSER_CORES configure option value \"$CROSSER_CORES\"" >&2
+    exit 1
+  fi
+  CROSSER_MAKEOPTIONS="-j $CROSSER_CORES"
+else
+  CROSSER_MAKEOPTIONS=""
+fi
+if test "x$DLLSPREFIX" = "x" && test "x$LSPREFIX" != "x" ; then
+  echo "Configuration variable LSPREFIX is deprecated. Please use DLLSPREFIX." >&2
+  DLLSPREFIX="$LSPREFIX"
+fi
+if test "x$DLLSPREFIX" = "x" && test "x$CROSSER_DST_PFX" != "x" ; then
+  echo "Configuration variable CROSSER_DST_PFX is deprecated. Please use DLLSPREFIX." >&2
+  DLLSPREFIX="$CROSSER_DST_PFX"
+fi
+if test "x$DLLSPREFIX" = "x" && test "x$PREFIX" != "x" ; then
+  echo "Configuration variable PREFIX is deprecated. Please use DLLSPREFIX." >&2
+  DLLSPREFIX="$PREFIX"
+fi
+if test "x$CROSSER_QT" = "x" ; then
+    CROSSER_QT="no"
+fi
+if test "x$CROSSER_QT" != "xyes" && test "x$CROSSER_QT" != "xno" ; then
+    echo "Unknown value \"$CROSSER_QT\" for CROSSER_QT. Valid values are \"yes\" and \"no\"" >&2
+    exit 1
+fi
