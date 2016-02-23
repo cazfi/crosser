4f07774699d605f48ad2d06e68ee38dae0a54e50
Marko Lindqvist <cazfi74@gmail.com>
2016-02-23 06:22:00 +0000

    Dropped obsolete readline_msdos.patch

--- dllstack.sh
+++ dllstack.sh
@@ -710,7 +710,6 @@ if ! build_component_full libtool libtool "" "" "" ""                 \
    ! free_src          PDCurses $VERSION_PDCURSES                        ||
    ! unpack_component  readline                                          ||
    ! patch_readline                                                      ||
-   ! patch_src readline $VERSION_READLINE "readline_msdos"               ||
    ! patch_src readline $VERSION_READLINE "readline_sighup"              ||
    ! patch_src readline $VERSION_READLINE "readline_statf"               ||
    ! patch_src readline $VERSION_READLINE "readline_pdcurses"            ||
