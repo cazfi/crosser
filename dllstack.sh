06aa6734b38ba62f45ce60e9b118cfa876c15089
Marko Lindqvist <cazfi74@gmail.com>
2017-03-11 20:28:02 +0200

    Update gtk3 to version 3.22.9
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dllstack.sh
+++ dllstack.sh
@@ -880,8 +880,8 @@ if ! build_component  gdk-pixbuf "--enable-relocations"               ||
    ! ( is_smaller_version $VERSION_GTK3 3.20.0 ||
        is_minimum_version $VERSION_GTK3 3.21.1 ||
        patch_src gtk+ $VERSION_GTK3 gtk3_demoless-3.20 )              ||
-   ! ( is_smaller_version $VERSION_GTK3 3.21.1 ||
-       patch_src gtk+ $VERSION_GTK3 gtk3_demoless-3.21 )              ||
+   ! ( is_smaller_version $VERSION_GTK3 3.22.9 ||
+       patch_src gtk+ $VERSION_GTK3 gtk3_demoless-3.22.9 )            ||
    ! ( is_smaller_version $VERSION_GTK3 3.18.0 ||
        patch_src gtk+ $VERSION_GTK3 gtk3_wm_macros )                  ||
    ! PKG_CONFIG_FOR_BUILD="$(host_pkg_config)"                        \
