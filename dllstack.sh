f1e18281197cd94390a780653c096dbaea0b2f75
Marko Lindqvist <cazfi74@gmail.com>
2017-10-08 17:07:17 +0300

    Fix build on systems without xlocale.h
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dllstack.sh
+++ dllstack.sh
@@ -678,7 +678,8 @@ if ! unpack_component     autoconf                          ||
    ! (is_smaller_version $VERSION_ICU 59.1 ||
       (patch_src icu $VERSION_ICU icu_uloc_slash &&
        patch_src icu $VERSION_ICU icu_mbstowcs_params &&
-       patch_src icu $VERSION_ICU icu_filetools_inc ))                    ||
+       patch_src icu $VERSION_ICU icu_filetools_inc ))                      ||
+   ! patch_src icu $VERSION_ICU icu_xlocale_no                              ||
    ! CXX="g++" CFLAGS="-fPIC" build_component_full native-icu4c icu4c ""    \
      "native" "icu/source"                                                  ||
    ! unpack_component tiff                                                  ||
