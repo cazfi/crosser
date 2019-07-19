9a6785988b2f377dcd1f916bd9294929a16c4d48
Marko Lindqvist <cazfi74@gmail.com>
2019-07-19 20:29:47 +0300

    Upgrade gtk4 to version 3.94.0
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dllstack.sh
+++ dllstack.sh
@@ -1194,7 +1194,9 @@ if ! unpack_component  graphene                                         ||
          patch_src gtk+ $VERSION_GTK4 "gtk4_gcr_find" ))                ||
    ! (is_minimum_version $VERSION_GTK4 3.92.0 ||
       build_component   gtk4                                          \
-      "--with-included-immodules --disable-cups GLIB_COMPILE_RESOURCES=$NATIVE_PREFIX/bin/glib-compile-resources" )                    ||
+      "--with-included-immodules --disable-cups GLIB_COMPILE_RESOURCES=$NATIVE_PREFIX/bin/glib-compile-resources" )                                     ||
+   ! (is_smaller_version $VERSION_GTK4 3.94.0 ||
+      patch_src gtk+ $VERSION_GTK4 "gtk4_lowercase_windows_h" )         ||
    ! (is_smaller_version $VERSION_GTK4 3.92.0 ||
       build_with_meson gtk4 \
       "-D enable-x11-backend=false -D enable-wayland-backend=false -D enable-win32-backend=true -D introspection=false -D with-included-immodules=all -D media=none" ) ||
