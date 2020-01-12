6007491a5567737f9c8f56a3a213a9bb757bae31
Marko Lindqvist <cazfi74@gmail.com>
2020-01-12 20:45:20 +0200

    Drop partial support for older Qt versions
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dllstack.sh
+++ dllstack.sh
@@ -2,7 +2,7 @@
 
 # dllstack.sh: Cross-compile set of libraries for Windows target.
 #
-# (c) 2008-2019 Marko Lindqvist
+# (c) 2008-2020 Marko Lindqvist
 #
 # This program is licensed under Gnu General Public License version 2.
 #
@@ -1267,16 +1267,11 @@ if ! unpack_component qt-everywhere-src                                ||
    ! patch_src qt-everywhere-src $VERSION_QT "qt_sharappidinfolink"    ||
    ! patch_src qt-everywhere-src $VERSION_QT "qt_g++"                  ||
    ! patch_src qt-everywhere-src $VERSION_QT "qt_disableidc-5.4.2"     ||
-   ! (is_smaller_version $VERSION_QT 5.11 ||
-      (patch_src qt-everywhere-src $VERSION_QT "qt_linkflags-5.11" &&
-       patch_src qt-everywhere-src $VERSION_QT "qt_vs_interop-5.11" &&
-       patch_src qt-everywhere-src $VERSION_QT "qt_dllsprefix-5.11" &&
-       patch_src qt-everywhere-src $VERSION_QT "qt_winextras_disable" )) ||
-   ! (is_smaller_version $VERSION_QT 5.12 ||
-      patch_src qt-everywhere-src $VERSION_QT "qt_yarr_inc_conflict" )   ||
-   ! (is_smaller_version $VERSION_QT 5.12 ||
-      is_minimum_version $VERSION_QT 5.12.5 ||
-      patch_src qt-everywhere-src $VERSION_QT "qt_nogl_fix" )            ||
+   ! patch_src qt-everywhere-src $VERSION_QT "qt_linkflags-5.11"         ||
+   ! patch_src qt-everywhere-src $VERSION_QT "qt_vs_interop-5.11"        ||
+   ! patch_src qt-everywhere-src $VERSION_QT "qt_dllsprefix-5.11"        ||
+   ! patch_src qt-everywhere-src $VERSION_QT "qt_winextras_disable"      ||
+   ! patch_src qt-everywhere-src $VERSION_QT "qt_yarr_inc_conflict"      ||
    ! (is_smaller_version $VERSION_QT 5.13 ||
       patch_src qt-everywhere-src $VERSION_QT "qt_localtime_not_r" )     ||
    ! patch_src qt-everywhere-src $VERSION_QT "qt_python3"                ||
