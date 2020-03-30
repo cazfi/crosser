38ee5ef54b1aeddd73ecd9f29a685e2532e34939
Marko Lindqvist <cazfi74@gmail.com>
2020-03-30 03:52:33 +0300

    Drop CROSSER_EXPAT, CROSSER_SDL entries from crosser.txt
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dllstack.sh
+++ dllstack.sh
@@ -1249,11 +1249,6 @@ log_write 1 "Creating crosser.txt"
   else
     echo "CROSSER_GTK2=\"no\""
   fi
-  echo
-  echo "# Already removed"
-  echo "# -------------------------"
-  echo "CROSSER_EXPAT=\"no\""
-  echo "CROSSER_SDL=\"no\""
 ) > "$DLLSPREFIX/crosser.txt"
 
 log_write 1 "Copying license information"
