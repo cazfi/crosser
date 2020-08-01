4d579644c2fb5252f725e8c52b6ec01ed56b1735
Marko Lindqvist <cazfi74@gmail.com>
2020-08-01 22:17:15 +0300

    harfbuzz: Build with meson
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dllstack.sh
+++ dllstack.sh
@@ -939,7 +939,7 @@ if ! build_component   tiff                                                 ||
        patch_src harfbuzz $VERSION_HARFBUZZ "harfbuzz_pthread_disable" )    ||
    ! ( is_minimum_version $VERSION_HARFBUZZ 2.6.7 ||
        patch_src       harfbuzz   $VERSION_HARFBUZZ "harfbuzz_python3" )    ||
-   ! build_component   harfbuzz   "--without-icu"                           ||
+   ! build_with_meson  harfbuzz   "-Dicu=disabled -Dfontconfig=disabled"    ||
    ! free_component    harfbuzz   $VERSION_HARFBUZZ "harfbuzz"              ||
    ! unpack_component  fontconfig                                           ||
    ! ( is_smaller_version $VERSION_FONTCONFIG 2.12.3 ||
