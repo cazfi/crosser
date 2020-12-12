bf8f3656bc6259e88a7e671eca5f42d4f923388d
Marko Lindqvist <cazfi74@gmail.com>
2020-12-12 04:34:36 +0200

    Implement parts of Qt-5.15 support
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dllstack.sh
+++ dllstack.sh
@@ -1227,7 +1227,9 @@ if ! unpack_component qt-everywhere-src                                ||
    ! (is_smaller_version $VERSION_QT5 5.15 ||
       patch_src qt-everywhere-src $VERSION_QT5 "qt_python3-5.15" )           ||
    ! (is_smaller_version $VERSION_QT5 5.15 ||
-      patch_src qt-everywhere-src $VERSION_QT5 "qt_testlib_disable" )        ||
+      patch_src qt-everywhere-src $VERSION_QT5 "qt_test_thread_disable" )    ||
+   ! (is_smaller_version $VERSION_QT5 5.15 ||
+      patch_src qt-everywhere-src $VERSION_QT5 "qt_nothr_shellexecute" )     ||
    ! (is_smaller_version $VERSION_QT5 5.14.0 ||
       is_minimum_version $VERSION_QT5 5.14.2 ||
       patch_src qt-everywhere-src $VERSION_QT5 "qt_setupapi_case" )          ||
