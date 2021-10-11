90eeeea5bd54871929153efdb044f423747b4938
Marko Lindqvist <cazfi74@gmail.com>
2021-10-11 06:39:14 +0300

    Upgrade gtk4 to version 4.4.0
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dllstack.sh
+++ dllstack.sh
@@ -1142,8 +1142,12 @@ if ! unpack_component  graphene                                         ||
    ! unpack_component  gtk4                                                ||
    ! patch_src gtk  $VERSION_GTK4 "gtk4_winnt"                             ||
    ! patch_src gtk  $VERSION_GTK4 "gtk4_lowercase_windows_h"               ||
-   ! build_with_meson gtk4 \
-     "-D x11-backend=false -D wayland-backend=false -D win32-backend=true -D introspection=disabled -D build-tests=false" ||
+   ! (is_minimum_version $VERSION_GTK4 4.4.0 ||
+      build_with_meson gtk4 \
+        "-D x11-backend=false -D wayland-backend=false -D win32-backend=true -D introspection=disabled -D build-tests=false" ) ||
+   ! (is_smaller_version $VERSION_GTK4 4.4.0 ||
+      build_with_meson gtk4 \
+        "-D x11-backend=false -D wayland-backend=false -D win32-backend=true -D introspection=disabled -D build-tests=false -D media-gstreamer=disabled" ) ||
    ! free_component    gtk        $VERSION_GTK4 "gtk4"
 then
   log_error "gtk4 chain build failed"
