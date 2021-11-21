49bc676146fd1247106863aef6edb09cc210bc9d
Marko Lindqvist <cazfi74@gmail.com>
2021-11-21 12:36:32 +0200

    Restore pcre to build
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- scripts/download_packets.sh
+++ scripts/download_packets.sh
@@ -432,6 +432,7 @@ then
     libpng)      VERSION_PNG=$VERSION_SELECTED ;;
     hicolor-icon-theme) VERSION_HICOLOR=$VERSION_SELECTED ;;
     libepoxy)    VERSION_LIBEPOXY=$VERSION_SELECTED ;;
+    pcre)        VERSION_PCRE=$VERSION_SELECTED ;;
     pcre2)       VERSION_PCRE2=$VERSION_SELECTED ;;
     sfml)        VERSION_SFML=$VERSION_SELECTED ;;
     fribidi)     VERSION_FRIBIDI=$VERSION_SELECTED ;;
@@ -579,6 +580,8 @@ download_needed "$MIRROR_SOURCEFORGE/projects/libpng/files/$PNG_DIR/$VERSION_PNG
                 "$MIRROR_SOURCEFORGE/projects/libpng/files/$PNG_DIR/older-releases/$VERSION_PNG/"
 RET="$RET $?"
 download_needed "$MIRROR_SOURCEFORGE/projects/libpng/files/zlib/$VERSION_ZLIB/" "zlib"       "$VERSION_ZLIB"       "$ZLIB_PACK"
+RET="$RET $?"
+download_needed "$MIRROR_SOURCEFORGE/projects/pcre/files/pcre/$VERSION_PCRE/" "pcre" "$VERSION_PCRE" "tar.bz2"
 RET="$RET $?"
 download_needed "https://github.com/PhilipHazel/pcre2/releases/download/pcre2-$VERSION_PCRE2/" "pcre2" "$VERSION_PCRE2" "tar.bz2" "https://ftp.pcre.org/pub/pcre/"
 RET="$RET $?"
