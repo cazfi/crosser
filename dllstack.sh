7bfb34904fbd125e85c530faadfaf20b5ea5f3a2
Marko Lindqvist <cazfi74@gmail.com>
2021-12-28 17:10:29 +0200

    Upgrade gettext to version 0.21
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dllstack.sh
+++ dllstack.sh
@@ -930,6 +930,8 @@ if ! build_component_full libtool libtool "" "" "" ""                 \
    ! (is_smaller_version $VERSION_GETTEXT 0.20 ||
       is_minimum_version $VERSION_GETTEXT 0.20.2 ||
       patch_src gettext $VERSION_GETTEXT "gettext_pthread_test_disable" )            ||
+   ! (is_smaller_version $VERSION_GETTEXT 0.21 ||
+      patch_src gettext $VERSION_GETTEXT "gettext_fs_ruby" )                         ||
    ! LIBS="-liconv" build_component gettext                                          \
      "$GETTEXT_VARS --enable-relocatable --enable-threads=windows --disable-libasprintf --without-emacs"    ||
    ! free_component    gettext    $VERSION_GETTEXT "gettext"                         ||
