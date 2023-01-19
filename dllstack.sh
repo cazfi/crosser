70111ebc419b242db1749561f153bfa621bac19b
Marko Lindqvist <cazfi74@gmail.com>
2023-01-19 16:06:29 +0200

    gtk-doc: Build >= 1.33 with meson
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- dllstack.sh
+++ dllstack.sh
@@ -833,9 +833,12 @@ if ! unpack_component     meson "" "meson/${VERSION_MESON}"              ||
    ! unpack_component     gtk-doc                                           ||
    ! patch_src gtk-doc $VERSION_GTK_DOC "gtkdoc_pc"                         ||
    ! patch_src gtk-doc $VERSION_GTK_DOC "gtkdoc_configheaders"              ||
-   ! autogen_component    gtk-doc $VERSION_GTK_DOC                          ||
-   ! build_component_host gtk-doc                                           ||
-   ! deldir_component  gtk-doc   $VERSION_GTK_DOC                             \
+   ! ( is_minimum_version "$VERSION_GTK_DOC" 1.33 ||
+       ( autogen_component    gtk-doc "$VERSION_GTK_DOC" &&
+         build_component_host gtk-doc ) )                                   ||
+   ! ( is_smaller_version "$VERSION_GTK_DOC" 1.33 ||
+       build_with_meson_host gtk-doc )                                      ||
+   ! deldir_component  gtk-doc   "$VERSION_GTK_DOC"                         \
      "native-gtk-doc"                                                       ||
    ! unpack_component     gobject-introspection                             ||
    ! build_with_meson_host gobject-introspection                            ||
