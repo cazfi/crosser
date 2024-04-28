7cda66404dc3a506dd0556f59593752c0c2cbb6e
Marko Lindqvist <cazfi74@gmail.com>
2024-04-29 02:53:06 +0300

    Update container to Ubuntu Noble (24.04 LTS)
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- docker/input/entry.sh
+++ docker/input/entry.sh
@@ -2,9 +2,11 @@
 
 # entry.sh : Entry point for the crosser building container 
 #
-# (c) 2023 Marko Lindqvist
+# (c) 2023-2024 Marko Lindqvist
 #
 # This program is licensed under Gnu General Public License version 2.
 #
 
-su - crosser /build/crosser_build.sh "${CROSSER_VSET}" "${CROSSER_SETUP}" "${CROSSER_TMPDIR}"
+CROSSER_DOCKER_USER="$(cat /build/username.txt)"
+
+su - "${CROSSER_DOCKER_USER}" /build/crosser_build.sh "${CROSSER_VSET}" "${CROSSER_SETUP}" "${CROSSER_TMPDIR}"
